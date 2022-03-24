314cce698bcb5e34eb4b5514d8404558263270ae
Bess Sadler <bess.sadler@princeton.edu>
2022-03-24 16:44:17 -0400

    Fix coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,10 @@
 # the additional setup, and require it from the spec files that actually need
 # it.
 #
+
+require "coveralls"
+Coveralls.wear!
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
