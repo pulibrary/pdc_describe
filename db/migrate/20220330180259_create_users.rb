f297a91aa4c465055a1ab80cb481e57da5c032cd
Hector Correa <hector_correa@princeton.edu>
2022-03-30 14:08:38 -0400

    Added user model and Omni Auth attributes

---
+++ db/migrate/20220330180259_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :netid
+      t.string :name
+      t.string :email
+      t.string :orcid
+      t.bool :super_admin
+
+      t.timestamps
+    end
+  end
+end
