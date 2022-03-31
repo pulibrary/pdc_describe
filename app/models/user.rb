e4e38f683e5b6b29d9d1b91b3fa59d0a166205d2
Hector Correa <hector_correa@princeton.edu>
2022-03-31 16:50:25 -0400

    Automatically create user record with CAS information

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,24 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-
-  # devise :database_authenticatable, :registerable,
-  #        :recoverable, :rememberable, :validatable,
-  #        :omniauthable
-
   devise :rememberable, :omniauthable
 
   def self.from_cas(access_token)
-    User.where(provider: access_token.provider, uid: access_token.uid).first
+    user = User.where(provider: access_token.provider, uid: access_token.uid).first
+    if user.nil?
+      # Create the user with some basic information from CAS.
+      #
+      # Other bits of information that we could use are:
+      #
+      #   access_token.extra.department (e.g. "Library - Information Technology")
+      #   access_token.extra.extra.departmentnumber (e.g. "41006")
+      #   access_token.extra.givenname (e.g. "Harriet")
+      #   access_token.extra.displayname (e.g. "Harriet Tubman")
+      #
+      user = User.new
+      user.provider = access_token.provider
+      user.uid = access_token.uid # this is the netid
+      user.email = access_token.extra.mail
+      user.save
+    end
+    user
   end
 end
