0b7dff2ae138086a66b9d00812ca892478ce6778
Bess Sadler <bess.sadler@princeton.edu>
2022-04-08 12:16:11 -0400

    Rubocop fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,3 @@
+# frozen_string_literal: true
 server "pdc-describe-prod1.princeton.edu", user: "deploy", roles: %w[app db web]
 server "pdc-describe-prod2.princeton.edu", user: "deploy", roles: %w[app db web]
