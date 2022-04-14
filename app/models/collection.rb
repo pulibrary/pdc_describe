689af2b13a793c40cce03cbdc79c461966c34380
Hector Correa <hector@hectorcorrea.com>
2022-04-14 15:23:31 -0400

    User default collection (#56)
    * Adds support for a default collection for users and initalizes it based on the department number in the CAS session
    
    * Added a few comments

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-# rubocop:disable Style/NumericPredicate
 class Collection < ApplicationRecord
+  # rubocop:disable Style/NumericPredicate
   def self.create_defaults
     return if Collection.count > 0
     Rails.logger.info "Creating default Collections"
@@ -10,5 +10,25 @@ class Collection < ApplicationRecord
     Collection.create(title: "Electronic Theses and Dissertations", code: "ETD")
     Collection.create(title: "Library Resources", code: "LIB")
   end
+  # rubocop:enable Style/NumericPredicate
+
+  # Returns the default collection.
+  # Used when we don't have anything else to determine a more specific collection for a user.
+  def self.default
+    Collection.where(code: "RD").first
+  end
+
+  # Returns the default collection for a given department number.
+  def self.default_for_department(department_number)
+    # Reference: https://docs.google.com/spreadsheets/d/1_Elxs3Ex-2wCbbKUzD4ii3k16zx36sYf/edit#gid=1484576831
+    if department_number.nil?
+      default
+    elsif department_number >= "31000" && department_number <= "31026"
+      Collection.where(code: "PPPL").first
+    elsif department_number >= "41000" && department_number <= "41032"
+      Collection.where(code: "LIB").first
+    else
+      default
+    end
+  end
 end
-# rubocop:enable Style/NumericPredicate
