d99ef570d59a9fe977abdf4f51cc3b5c12a93b8f
Hector Correa <hector_correa@princeton.edu>
2022-04-19 16:11:43 -0400

    MOAR tests

--- spec/models/dataset_spec.rb
+++ spec/models/dataset_spec.rb
@@ -2,13 +2,32 @@
 require "rails_helper"
 
 RSpec.describe Dataset, type: :model do
+  before { Collection.create_defaults }
+
+  let(:collection) { Collection.default }
   let(:user) { FactoryBot.create :user }
-  let(:collection) { FactoryBot.create :collection }
+  let(:user_other) { FactoryBot.create :user }
+  let(:superadmin_user) {
+    User.from_cas(OmniAuth::AuthHash.new(provider: "cas", uid: "fake1", extra: { mail: "fake@princeton.edu" }))
+  }
 
   it "creates a skeleton dataset and links it to a new work" do
     ds = described_class.create_skeleton("test title", user.id, collection.id)
     expect(ds.created_by_user.id).to eq user.id
     expect(ds.work.collection.id).to eq collection.id
     expect(ds.ark).to be_present
+  end
+
+  it "returns datasets waiting for approval depending on the user" do
+    my_ds = described_class.create_skeleton("test title", user.id, collection.id)
+    other_ds =  described_class.create_skeleton("test title", user_other.id, collection.id)
+
+    # Superadmins can approve pending works
+    awaiting = described_class.admin_datasets_by_user_state(superadmin_user, "AWAITING-APPROVAL")
+    expect(awaiting.count > 0).to be true
+
+    # Normal users don't get anything
+    awaiting = described_class.admin_datasets_by_user_state(user, "AWAITING-APPROVAL")
+    expect(awaiting.count).to be 0
   end
 end
