45ce8295145ebc521c087c07258316d80582074c
Hector Correa <hector_correa@princeton.edu>
2022-04-28 14:01:16 -0400

    MOAR tests

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -32,11 +32,12 @@ class CollectionsController < ApplicationController
       end
     else
       Rails.logger.warn("Unauthorized to update collection #{@collection.id} (current user: #{current_user.id})")
-      redirect_to collection_path(@collection)
+      redirect_to collections_path
     end
   end
   # rubocop:enable Metrics/MethodLength
 
+  # This is a JSON only endpoint
   def add_admin
     @collection = Collection.find(params[:id])
     if can_edit?
@@ -54,6 +55,7 @@ class CollectionsController < ApplicationController
     end
   end
 
+  # This is a JSON only endpoint
   def add_submitter
     @collection = Collection.find(params[:id])
     if can_edit?
@@ -71,21 +73,29 @@ class CollectionsController < ApplicationController
     end
   end
 
+  # This is a JSON only endpoint
+  # rubocop:disable Metrics/MethodLength
   def delete_user_from_collection
-    uid = params[:uid]
-    if uid == current_user.uid
-      render status: :bad_request, json: { message: "Cannot remove yourself from a collection. Contact a super-admin for help." }
-    else
-      collection_id = params[:id]
-      user = User.where(uid: uid).first
-      if user.nil?
-        render status: :bad_request, json: { message: "User was not found" }
+    @collection = Collection.find(params[:id])
+    if can_edit?
+      uid = params[:uid]
+      if uid == current_user.uid
+        render status: :bad_request, json: { message: "Cannot remove yourself from a collection. Contact a super-admin for help." }
       else
-        UserCollection.where(user_id: user.id, collection_id: collection_id).each(&:delete)
-        render status: :ok, json: { message: "OK" }
+        collection_id = params[:id]
+        user = User.where(uid: uid).first
+        if user.nil?
+          render status: :bad_request, json: { message: "User was not found" }
+        else
+          UserCollection.where(user_id: user.id, collection_id: collection_id).each(&:delete)
+          render status: :ok, json: { message: "OK" }
+        end
       end
+    else
+      render status: :unauthorized, json: { message: "Unauthorized" }
     end
   end
+  # rubocop:enable Metrics/MethodLength
 
   private
 
