725d85d68214007cd2e4e3c5df29119137a6ef49
jrgriffiniii <jrg5@princeton.edu>
2022-04-28 09:30:11 -0400

    [wip]

--- spec/factories/work.rb
+++ spec/factories/work.rb
@@ -7,5 +7,41 @@ FactoryBot.define do
       collection { FactoryBot.create(:research_data) }
       created_by_user_id { FactoryBot.create(:user).id }
     end
+
+    factory :us_national_pandemic_report_work do
+      title { "" }
+      collection { FactoryBot.create(:library_resources) }
+      created_by_user_id { FactoryBot.create(:user).id }
+    end
+    factory :fortune_100_blm_work do
+      title { "" }
+      collection { FactoryBot.create(:library_resources) }
+      created_by_user_id { FactoryBot.create(:user).id }
+    end
+    factory :racial_wealth_gap_work do
+      title { "" }
+      collection { FactoryBot.create(:library_resources) }
+      created_by_user_id { FactoryBot.create(:user).id }
+    end
+    factory :hungary_around_clock_work do
+      title { "" }
+      collection { FactoryBot.create(:library_resources) }
+      created_by_user_id { FactoryBot.create(:user).id }
+    end
+    factory :gu_dian_yan_jiu_work do
+      title { "" }
+      collection { FactoryBot.create(:library_resources) }
+      created_by_user_id { FactoryBot.create(:user).id }
+    end
+    factory :racism_inequality_health_care_work do
+      title { "" }
+      collection { FactoryBot.create(:library_resources) }
+      created_by_user_id { FactoryBot.create(:user).id }
+    end
+    factory :national_health_ukraine_work do
+      title { "" }
+      collection { FactoryBot.create(:library_resources) }
+      created_by_user_id { FactoryBot.create(:user).id }
+    end
   end
 end
