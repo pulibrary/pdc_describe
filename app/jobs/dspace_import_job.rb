8c700930fd8ded4739cc81e344de826af3079b08
jrgriffiniii <jrg5@princeton.edu>
2022-04-28 11:14:46 -0400

    [wip]

--- app/jobs/dspace_import_job.rb
+++ app/jobs/dspace_import_job.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
 
 class DSpaceImportJob < ApplicationJob
-  def perform(url:, user:, collection:, work_type: nil)
+  def perform(url:, user_id:, collection_id:, work_type: nil)
+    user = User.find(user_id)
+    collection = User.find(collection_id)
+
     service = DSpaceImportService.new(url: url, user: user, collection: collection, work_type: work_type)
     service.import!
   end
--- app/services/dspace_import_service.rb
+++ app/services/dspace_import_service.rb
@@ -1,34 +1,47 @@
-class DSpaceImportService
+# frozen_string_literal: true
+require "faraday"
+
+class DspaceImportService
   class MetadataDocument
+    attr_reader :attributes
 
     def initialize(document)
       @document = document
+      @attributes = {}
+
+      self.class.attribute_xpaths.each_pair do |attr, xpath|
+        elements = dc_element.xpath(xpath, self.class.namespaces)
+        attributes[attr] = elements.map(&:content)
+      end
     end
 
+    def dc_element
+      @dc_element ||= root.at_xpath("./oai:GetRecord/oai:record/oai:metadata/oai_dc:dc", self.class.namespaces)
+    end
+
+    delegate :each_pair, to: :attributes
     delegate :root, to: :@document
 
     def self.namespaces
       {
-        dc: ''
+        oai: "http://www.openarchives.org/OAI/2.0/",
+        oai_dc: "http://www.openarchives.org/OAI/2.0/oai_dc/",
+        dc: "http://purl.org/dc/elements/1.1/"
       }
     end
 
     def self.attribute_xpaths
       {
-        title: './dc:title'
+        title: "./dc:title",
+        creator: "./dc:creator",
+        subject: "./dc:subject",
+        date: "./dc:date",
+        identifier: "./dc:identifier",
+        language: "./dc:language",
+        relation: "./dc:relation",
+        publisher: "./dc:publisher"
       }
     end
-
-    def find_element(value)
-      attr_xpath = self.class.attribute_xpaths[value]
-      root.at_xpath(attr_xpath, self.class.namespaces)
-    end
-
-    def read_attribute(value)
-      element = find_element(value)
-      element.content
-    end
-
   end
 
   class Metadata
@@ -36,7 +49,7 @@ class DSpaceImportService
 
     def self.from_xml(source)
       document = MetadataDocument.new(source)
-      metadata = self.new
+      metadata = new
       document.attributes.each_pair do |key, value|
         metadata[key] = value
       end
@@ -46,23 +59,36 @@ class DSpaceImportService
 
     def initialize(attributes: {})
       @attributes = attributes
+
+      self.class.define_attribute_methods
     end
 
+    delegate :each_pair, to: :attributes
+
     def read_attribute(value)
-      send(value.snake_case.to_sym)
+      send(value.to_sym)
+    end
+
+    def []=(key, value)
+      send("#{key}=".to_sym, value)
     end
 
     def self.attribute_names
       [
-        :title
+        :title,
+        :creator,
+        :subject,
+        :date,
+        :identifier,
+        :language,
+        :relation,
+        :publisher
       ]
     end
 
-    private
-
-    def define_attribute_methods
-      self.class.attribute_names.each do |attr_name|
-        define_method attr_name.to_sym do |*args|
+    def self.define_attribute_methods
+      attribute_names.each do |attr_name|
+        define_method attr_name.to_sym do |*_args|
           attributes[attr_name]
         end
 
@@ -77,10 +103,12 @@ class DSpaceImportService
   class DublinCoreMetadata < Metadata
     def self.attribute_names
       super.merge([
-        :title
-      ])
+                    :title
+                  ])
     end
   end
+
+  attr_reader :url, :collection, :user, :work_type
 
   def initialize(url:, user:, collection:, work_type: nil)
     @url = url
@@ -90,14 +118,25 @@ class DSpaceImportService
   end
 
   def metadata
-    @metadata ||= Metadata.new(document)
+    @metadata ||= Metadata.from_xml(document)
   end
 
   delegate :title, to: :metadata
 
   def import!
-    metadata.each_pair do |field, value|
-      work.write_attribute(field, value)
+    request!
+
+    metadata.each_pair do |attr, value|
+      attr_type = Work.attribute_types[attr.to_s]
+      if value.is_a?(Enumerable) && attr_type.is_a?(ActiveModel::Type::String)
+        Rails.logger.warn("The value for the attribute `#{attr}` is an enumerable, but the Work Model defines this only for scalar string values. Importing only the first value into the attribute.")
+        work.write_attribute(attr, value.first)
+      else
+        work.write_attribute(attr, value)
+      end
+    rescue ActiveModel::MissingAttributeError
+      # raise(NotImplementedError, "Failed to import the attribute `#{attr}`: This attribute is not defined on the Work Model.")
+      Rails.logger.warn("Failed to import the attribute `#{attr}`: This attribute is not defined on the Work Model.")
     end
 
     work
@@ -109,12 +148,22 @@ class DSpaceImportService
 
   private
 
-  def request!
-    @response ||= client.get(url)
-  end
+    def client
+      Faraday.new(
+        url: url,
+        params: {},
+        headers: {
+          "Content-Type" => "application/xml"
+        }
+      )
+    end
 
-  def document
-    Nokogiri::XML.parse(@response.body)
-  end
+    def request!
+      # @response ||= client.get(@url)
+      @response ||= client.get
+    end
 
+    def document
+      Nokogiri::XML.parse(@response.body)
+    end
 end
