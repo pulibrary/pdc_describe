800e9f0ea35b2c01d0138d75be5b79188c7ed5ee
Hector Correa <hector_correa@princeton.edu>
2022-04-29 13:33:04 -0400

    Add db constrain on collection.code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_21_154837) do
+ActiveRecord::Schema.define(version: 2022_04_29_133012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2022_04_21_154837) do
     t.string "code"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["code"], name: "index_collections_on_code", unique: true
   end
 
   create_table "datasets", force: :cascade do |t|
