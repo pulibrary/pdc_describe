f386b527226f6ab0ab72c9b7629fb0c3337cb937
Hector Correa <hector@hectorcorrea.com>
2022-06-23 13:41:18 -0400

    Tuned the ORCID validation (#173)
    * Tuned the ORCID validation
    
    * Rubocop nitpicking

--- spec/models/orcid_spec.rb
+++ spec/models/orcid_spec.rb
@@ -4,6 +4,8 @@ require "rails_helper"
 RSpec.describe Orcid, type: :model do
   it "validates ORCIDs" do
     expect(described_class.valid?("1234-5678-9012-1234")).to be true
+    expect(described_class.valid?("1234-5678-9012-123X")).to be true
+    expect(described_class.valid?("x1234-5678-9012-1234")).to be false
     expect(described_class.valid?("01234-5678-9012-1234")).to be false
     expect(described_class.valid?("234-5678-9012-1234")).to be false
     expect(described_class.valid?("ABCD-5678-9012-1234")).to be false
