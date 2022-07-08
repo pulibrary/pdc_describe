1d3fa084cdc8d69ae8340b9773448e3c36705cd1
James R. Griffin III <jrg5@princeton.edu>
2022-07-08 12:12:41 -0400

    Separating the `orcid.js` from `application.js` and ensuring that `describe/` is prefixed to the Webpack manifest paths (#213)
    Co-authored-by: Hector Correa <hector@hectorcorrea.com>

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import { isOrcid } from './orcid'
-window.isOrcid = isOrcid;
 
 Rails.start()
 Turbolinks.start()
