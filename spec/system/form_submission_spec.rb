72196710978e4b2d4143763ba865455284140984
Bess Sadler <bess.sadler@princeton.edu>
2022-08-09 16:09:07 -0400

    System spec to test form submission (#265)
    Compile assets when running tests in browser

---
+++ spec/system/form_submission_spec.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe "Form submission for a legacy dataset", type: :system, mock_ezid_api: true do
+  # Notice that we manually create a user for this test (rather the one from FactoryBot)
+  # because we need to make sure the user also has a list of collections where they can
+  # submit works (UserCollection table) and the FactoryBot stub does not account for
+  # that where as creating a user via `User.from_cas()` does.
+  let(:user) do
+    hash = OmniAuth::AuthHash.new(provider: "cas", uid: "who", extra: { mail: "who@princeton.edu", departmentnumber: "31000" })
+    User.from_cas(hash)
+  end
+  let(:title) { "Sowing the Seeds for More Usable Web Archives: A Usability Study of Archive-It" }
+  let(:contributors) do
+    [
+      "Abrams, Samantha",
+      "Antracoli, Alexis",
+      "Appel, Rachel",
+      "Caust-Ellenbogen, Celia",
+      "Dennison, Sarah",
+      "Duncan, Sumitra",
+      "Ramsay, Stefanie"
+    ]
+  end
+  let(:issue_date) { 2019 }
+  let(:related_publication) { "Sowing the Seeds for More Usable Web Archives: A Usability Study of Archive-It, Fall/Winter 2019, Vol. 82, No. 2." }
+  let(:abstract) do
+    "In 2017, seven members of the Archive-It Mid-Atlantic Users Group (AITMA) conducted a study of 14 subjects representative of their stakeholder
+    populations to assess the usability of Archive-It, a web archiving subscription service of the Internet Archive. While Archive-It is the most
+    widely-used tool for web archiving, little is known about how users interact with the service. This study intended to teach us what users expect
+    from web archives, which exist as another form of archival material. End-user subjects executed four search tasks using the public Archive-It
+    interface and the Wayback Machine to access archived information on websites from the facilitators' own harvested collections and provide feedback
+    about their experiences. The tasks were designed to have straightforward pass or fail outcomes, and the facilitators took notes on the subjects'
+    behavior and commentary during the sessions. Overall, participants reported mildly positive impressions of Archive-It public user interface based
+    on their session. The study identified several key areas of improvement for the Archive-It service pertaining to metadata options, terminology display,
+    indexing of dates, and the site's search box."
+  end
+  let(:description) { "Download the README.txt for a detailed description of this dataset's content." }
+  let(:ark) { "http://arks.princeton.edu/ark:/88435/dsp01d791sj97j" }
+  let(:collection) { "Research Data" }
+
+  before do
+    stub_datacite(host: "api.datacite.org", body: datacite_register_body(prefix: "10.34770"))
+  end
+  context "happy path" do
+    it "produces and saves a valid datacite record", js: true do
+      sign_in user
+      visit new_work_path
+      fill_in "title_main", with: title
+
+      fill_in "given_name_1", with: "Samantha"
+      fill_in "family_name_1", with: "Abrams"
+      click_on "Create New"
+      fill_in "description", with: description
+      click_on "Additional Metadata"
+      fill_in "publication_year", with: issue_date
+      click_on "Save Work"
+      page.find(:xpath, "//input[@value='file_other']").choose
+      click_on "Continue"
+      click_on "Continue"
+      click_on "Complete"
+
+      expect(page).to have_content "AWAITING-APPROVAL"
+    end
+  end
+end
