04dd41e2e106afed62c3be030f88e23ea1547a36
Carolyn Cole <cac9@princeton.edu>
2022-08-29 14:50:15 -0400

    Changing metadata to be jsonb
    fixes #342

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_23_094818) do
+ActiveRecord::Schema.define(version: 2022_08_29_184502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -123,7 +123,7 @@ ActiveRecord::Schema.define(version: 2022_08_23_094818) do
     t.integer "created_by_user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.json "metadata"
+    t.jsonb "metadata"
     t.string "profile"
     t.text "location_notes"
     t.text "submission_notes"
