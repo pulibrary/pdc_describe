b5f18f283d4b340cc01de7dab50c2111bb8a96e2
Bess Sadler <bess.sadler@princeton.edu>
2022-08-29 16:53:09 -0400

    WIP: Ensure a submitter cannot edit another submitter's file

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,13 +1,21 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :user do
+  factory :user, class: "User" do
     sequence(:uid) { FFaker::InternetSE.login_user_name }
     sequence(:email) { FFaker::InternetSE.email }
-    default_collection_id { Collection.research_data.id }
+    # default_collection_id { Collection.research_data.id }
     full_name { FFaker::Name.name }
     display_name { full_name.split(" ").first }
     provider { :cas }
+
+    factory :princeton_submitter do
+      default_collection_id { Collection.default_for_department("12345").id }
+    end
+
+    factory :pppl_submitter do
+      default_collection_id { Collection.default_for_department("31000").id }
+    end
   end
 
   factory :super_admin_user, class: "User" do
@@ -15,4 +23,10 @@ FactoryBot.define do
     sequence(:email) { "fake1@princeton.edu" }
     provider { :cas }
   end
+
+  # After a user is created, their ID is added to the collections where they can deposit
+  after(:create) do |user|
+    user.setup_user_default_collections
+  end
+  
 end
---
+++ spec/system/authz_submitter_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+require "rails_helper"
+##
+# A submitter is a logged in user with no permissions other than being able to deposit.
+# One submitter should not be able to edit the work of another submitter.
+RSpec.describe "Authz for submitters", type: :system, js: true, mock_ezid_api: true do
+  describe "A submitter" do
+    let(:submitter1) { FactoryBot.create :princeton_submitter }
+    let(:submitter2) { FactoryBot.create :princeton_submitter }
+
+    before do
+      stub_s3
+      stub_datacite(host: "api.datacite.org", body: datacite_register_body(prefix: "10.34770"))
+    end
+
+    it "can submit" do
+      sign_in submitter1
+      visit user_path(submitter1)
+      expect(page).to have_content submitter1.display_name
+      click_on "Submit New"
+      fill_in "title_main", with: FFaker::Book.title
+
+      fill_in "given_name_1", with: FFaker::Name.first_name
+      fill_in "family_name_1", with: FFaker::Name.last_name
+      click_on "Create New"
+      fill_in "description", with: FFaker::Lorem.paragraph
+      click_on "Additional Metadata"
+      expect(page).to have_content "Research Data"
+      click_on "Save Work"
+      page.find(:xpath, "//input[@value='file_other']").choose
+      click_on "Continue"
+      click_on "Continue"
+      click_on "Complete"
+
+      expect(page).to have_content "awaiting_approval"
+
+      work = Work.last
+
+      # Submitter can edit their own work
+      visit edit_work_path(work)
+      fill_in "title_main", with: FFaker::Book.title
+      click_on "Save Work"
+byebug
+      sign_out submitter1
+      sign_in submitter2
+
+      visit edit_work_path(work)
+      byebug
+      fill_in "title_main", with: FFaker::Book.title
+      click_on "Save Work"
+
+      byebug
+    end
+  end
+end
