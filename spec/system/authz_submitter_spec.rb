8a62cfe23a7b1bc388477f7e0a177f65e566f2bc
Bess Sadler <bess.sadler@princeton.edu>
2022-08-30 08:38:20 -0400

    Refine test

--- spec/system/authz_submitter_spec.rb
+++ spec/system/authz_submitter_spec.rb
@@ -7,18 +7,21 @@ RSpec.describe "Authz for submitters", type: :system, js: true, mock_ezid_api: t
   describe "A submitter" do
     let(:submitter1) { FactoryBot.create :princeton_submitter }
     let(:submitter2) { FactoryBot.create :princeton_submitter }
+    let(:title1) { "Title One" }
+    let(:title2) { "Title Two" }
+    let(:title3) { "Title Three" }
 
     before do
       stub_s3
       stub_datacite(host: "api.datacite.org", body: datacite_register_body(prefix: "10.34770"))
     end
 
-    it "can submit" do
+    it "one submitter should not be able to edit the work of another submitter" do
       sign_in submitter1
       visit user_path(submitter1)
       expect(page).to have_content submitter1.display_name
       click_on "Submit New"
-      fill_in "title_main", with: FFaker::Book.title
+      fill_in "title_main", with: title1
 
       fill_in "given_name_1", with: FFaker::Name.first_name
       fill_in "family_name_1", with: FFaker::Name.last_name
@@ -33,23 +36,23 @@ RSpec.describe "Authz for submitters", type: :system, js: true, mock_ezid_api: t
       click_on "Complete"
 
       expect(page).to have_content "awaiting_approval"
-
+byebug
       work = Work.last
 
       # Submitter can edit their own work
       visit edit_work_path(work)
-      fill_in "title_main", with: FFaker::Book.title
+      fill_in "title_main", with: title2
       click_on "Save Work"
 byebug
       sign_out submitter1
       sign_in submitter2
 
       visit edit_work_path(work)
-      byebug
-      fill_in "title_main", with: FFaker::Book.title
+byebug
+      fill_in "title_main", with: title3
       click_on "Save Work"
 
-      byebug
+byebug
     end
   end
 end
