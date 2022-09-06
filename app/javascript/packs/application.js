150f24af29f2c9a3b5d1d44228157031d36913c2
James R. Griffin III <jrg5@princeton.edu>
2022-09-06 15:43:03 -0400

    Prototyping the WorkForm JavaScript functionality, which integrates drag-and-drop reordering support for Work upload <form> <table> row elements (#375)
    * Prototyping the WorkForm JavaScript functionality, which integrates drag-and-drop reordering support for Work upload <form> <table> row elements
    
    * Ensuring that the `jQuery.rails` JavaScript Global object does *not* break conflicting JS functionality when jQuery plug-ins are loaded

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,19 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+// Provides @mention functionality in textboxes (adds to jQuery UI autocomplete)
+import "./vendor/jquery-ui-triggeredAutocomplete"
+import WorkForm from "./works/form"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$(document).ready( (event) => {
+
+  $(".work-form").each( (index, element) => {
+    const $element = $(element);
+    const work = $element.data('work');
+    const workForm = new WorkForm($element, work);
+  });
+});
