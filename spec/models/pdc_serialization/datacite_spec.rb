43af3b311d925121a3a81cf582398d1fcfe68b8d
jrgriffiniii <jrg5@princeton.edu>
2022-09-30 16:02:53 -0400

    Removing DspaceImportJob and HumanizedDiskService classes; Increasing the test coverage to 100%

--- spec/models/pdc_serialization/datacite_spec.rb
+++ spec/models/pdc_serialization/datacite_spec.rb
@@ -31,6 +31,18 @@ RSpec.describe PDCSerialization::Datacite, type: :model do
     end
   end
 
+  describe ".new_from_work" do
+    let(:work) { FactoryBot.create(:shakespeare_and_company_work) }
+    let(:new_resource) { described_class.new_from_work(work) }
+    it "creates a new Resource from a Work" do
+      expect(new_resource).to be_a(PDCSerialization::Datacite)
+      expect(new_resource.mapping).to be_a(Datacite::Mapping::Resource)
+      expect(new_resource.mapping.titles).not_to be_empty
+      expect(new_resource.mapping.titles.first).to be_a(Datacite::Mapping::Title)
+      expect(new_resource.mapping.titles.first.value).to eq("Shakespeare and Company Project Dataset: Lending Library Members, Books, Events")
+    end
+  end
+
   context "create a datacite record through a form submission" do
     let(:doi) { "https://doi.org/10.34770/pe9w-x904" }
     let(:work_resource) do
@@ -134,6 +146,60 @@ RSpec.describe PDCSerialization::Datacite, type: :model do
         it "Workflow" do
           resource_type = described_class.datacite_resource_type("Workflow")
           expect(resource_type.resource_type_general.value).to eq "Workflow"
+        end
+        it "Other" do
+          resource_type = described_class.datacite_resource_type("Other")
+          expect(resource_type.resource_type_general.value).to eq "Other"
+        end
+      end
+
+      context "with multiple titles" do
+        let(:resource_titles) do
+          [
+            PDCMetadata::Title.new(title: "example subtitle", title_type: "Subtitle"),
+            PDCMetadata::Title.new(title: "example alternative title", title_type: "AlternativeTitle"),
+            PDCMetadata::Title.new(title: "example translated title", title_type: "TranslatedTitle")
+          ]
+        end
+        let(:work_resource) do
+          json = {
+            "doi": doi,
+            "identifier_type": "DOI",
+            "titles": resource_titles,
+            "description": "All data is related to the Shakespeare and Company bookshop and lending library opened and operated by Sylvia Beach in Paris, 1919–1962.",
+            "creators": [
+              { "value": "Kotin, Joshua", "name_type": "Personal", "given_name": "Joshua", "family_name": "Kotin", "affiliations": [], "sequence": "1" }
+            ],
+            "resource_type": "Dataset",
+            "publisher": "Princeton University",
+            "publication_year": "2020"
+          }.to_json
+          PDCMetadata::Resource.new_from_json(json)
+        end
+
+        it "has titles" do
+          titles = mapping.titles
+
+          expect(titles).to be_an(Array)
+          expect(titles.length).to eq(3)
+
+          subtitle_title = titles.first
+          expect(subtitle_title).to be_a(::Datacite::Mapping::Title)
+          expect(subtitle_title.value).to eq("example subtitle")
+          expect(subtitle_title.type).to be_a(Datacite::Mapping::TitleType)
+          expect(subtitle_title.type.value).to eq("Subtitle")
+
+          alt_title = titles[1]
+          expect(alt_title).to be_a(::Datacite::Mapping::Title)
+          expect(alt_title.value).to eq("example alternative title")
+          expect(alt_title.type).to be_a(Datacite::Mapping::TitleType)
+          expect(alt_title.type.value).to eq("AlternativeTitle")
+
+          translated_title = titles.last
+          expect(translated_title).to be_a(::Datacite::Mapping::Title)
+          expect(translated_title.value).to eq("example translated title")
+          expect(translated_title.type).to be_a(Datacite::Mapping::TitleType)
+          expect(translated_title.type.value).to eq("TranslatedTitle")
         end
       end
     end
