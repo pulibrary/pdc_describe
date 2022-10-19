2cc8749af1c35a508b08816aea8d60d512830ac6
jrgriffiniii <jrg5@princeton.edu>
2022-10-19 08:40:41 -0400

    Ensuring that ActiveMailer is disabled for the production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,9 +67,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_options = {
-    from: "noreply@datacommons.princeton.edu"
-  }
+  config.action_mailer.delivery_method = :test
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
