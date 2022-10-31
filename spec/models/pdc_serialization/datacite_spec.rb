a528d012c67ddc144d27cd7bb702a375240622d3
Bess Sadler <bess.sadler@princeton.edu>
2022-10-31 13:55:42 -0400

    Add another missing related object to cytoskeletal migration test

--- spec/models/pdc_serialization/datacite_spec.rb
+++ spec/models/pdc_serialization/datacite_spec.rb
@@ -240,9 +240,10 @@ RSpec.describe PDCSerialization::Datacite, type: :model do
     let(:parsed_xml) { Datacite::Mapping::Resource.parse_xml(datacite) }
 
     it "references the related object in the datacite record" do
-      expect(parsed_xml.related_identifiers.count).to eq 2
-      expect(parsed_xml.related_identifiers.first.identifier_type.value).to eq "ARK"
-      expect(parsed_xml.related_identifiers.last.identifier_type.value).to eq "arXiv"
+      expect(parsed_xml.related_identifiers.count).to eq 3
+      expect(parsed_xml.related_identifiers[0].identifier_type.value).to eq "ARK"
+      expect(parsed_xml.related_identifiers[1].identifier_type.value).to eq "arXiv"
+      expect(parsed_xml.related_identifiers[2].identifier_type.value).to eq "DOI"
     end
   end
 end
