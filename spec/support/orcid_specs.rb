d47e631cd44a30790a8d52101156df3e764e2b47
Hector Correa <hector@hectorcorrea.com>
2022-11-02 10:02:31 -0400

    ORCID for collaborators/contributors (#588)
    * Fixes bug with ORCID fetching for contributors
    
    * Added a couple of tests
    
    * Linting errors
    
    * Minor comment tweak

--- spec/support/orcid_specs.rb
+++ spec/support/orcid_specs.rb
@@ -4,7 +4,7 @@ require "sinatra/base"
 # This sets up a little local server to respond to ORCID ajax requests
 # This is utilized to test the javascript that parses the ajax response and puts the ORCID family name and given name into the form
 #  The data returned is a simplified version of the data returned from ORCID.  If more data than just the name is needed the data here will need to be updated
-#  Given any ORCID the response value is hard coded to Sally Smith
+#  The response is hard-coded for a few known values and defaults to "Sally Smith" for all others.
 #
 # This Fake was modeled on the information in this article: https://thoughtbot.com/blog/using-capybara-to-test-javascript-that-makes-http
 class FakeOrcidIntegration < Sinatra::Base
@@ -16,9 +16,30 @@ class FakeOrcidIntegration < Sinatra::Base
   get "/:orcid" do |orcid|
     content_type(:js)
     callback = params[:callback]
-    data = { "orcid-identifier" => { "uri" => "http://orcid.org/#{orcid}", "path" => orcid, "host" => "orcid.org" },
-             "person" => { "name" => { "given-names" => { "value" => "Sally" }, "family-name" => { "value" => "Smith" } } } }.to_json
-    "#{callback}(#{data});"
+    data = {
+      "orcid-identifier" => {
+        "uri" => "http://orcid.org/#{orcid}",
+        "path" => orcid,
+        "host" => "orcid.org"
+      },
+      "person" => {
+        "name" => {
+          "given-names" => { "value" => "Sally" },
+          "family-name" => { "value" => "Smith" }
+        }
+      }
+    }
+
+    case orcid
+    when "0000-0001-8965-6820"
+      data["person"]["name"]["given-names"]["value"] = "Carmen"
+      data["person"]["name"]["family-name"]["value"] = "Valdez"
+    when "0000-0001-5443-5964"
+      data["person"]["name"]["given-names"]["value"] = "Melody"
+      data["person"]["name"]["family-name"]["value"] = "Loya"
+    end
+
+    "#{callback}(#{data.to_json});"
   end
 end
 
