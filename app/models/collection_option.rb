79a84f0091ad6075f6b00198fe71b03cbb8d39a5
James R. Griffin III <jrg5@princeton.edu>
2022-11-02 13:35:52 -0400

    Implementing support for the enabling and disabling of e-mail message notifications from members of Collections to Users (#574)
    * Implementing support for the enabling and disabling of e-mail message
    notifications from members of Collections to Users
    
    * Addressing formatting errors; Ensuring that User#disable_messages_from and Collection#disable_messages_for uses ActiveRecord::Associations::CollectionProxy#destroy for removing records on the join table
    
    * Refactoring the many-to-many relationships between Users and Collections
    for supporting e-mail messages for notifications by implementing the
    CollectionOption Model
    
    * Introducing corrections to the UserController, Collection, and User
    Models
    
    * Removing the RuboCop override from UsersController
    
    * Removing the unnecessary test suites for User#enable_messages_for and Collection#enable_messages_from methods
    
    * Restoring the RuboCop override for UsersController#update
    
    Co-authored-by: carolyncole <1599081+carolyncole@users.noreply.github.com>

---
+++ app/models/collection_option.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+class CollectionOption < ApplicationRecord
+  EMAIL_MESSAGES = 0
+
+  belongs_to :collection
+  belongs_to :user
+
+  enum option_type: { email_messages: 0 }
+
+  # Provides a human-readable label for the type of the option
+  # @note This should perhaps in the future parse from a YAML config. file
+  # @return [String] the label
+  def self.option_type_labels
+    {
+      email_messages: "E-mail messages for Collection notifications"
+    }
+  end
+
+  # Finds the label for the option type value set for a given Model
+  # @param [Integer] value the option type value
+  # @return [String] the label
+  def self.find_option_type_label(value)
+    option_type_labels.fetch(value.to_sym, nil)
+  end
+
+  # Finds the label for the option type
+  # @return [String] the label
+  def option_type_label
+    self.class.find_option_type_label(option_type)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_19_171715) do
+ActiveRecord::Schema.define(version: 2022_10_31_150209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,15 @@ ActiveRecord::Schema.define(version: 2022_10_19_171715) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "collection_options", force: :cascade do |t|
+    t.integer "option_type"
+    t.integer "option_value"
+    t.bigint "collection_id"
+    t.bigint "user_id"
+    t.index ["collection_id"], name: "index_collection_options_on_collection_id"
+    t.index ["user_id"], name: "index_collection_options_on_user_id"
   end
 
   create_table "collections", force: :cascade do |t|
