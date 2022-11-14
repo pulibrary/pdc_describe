c1a83f7083ac40a04581734e9000bc8b6f1e0e4b
Tyler Wade <tw8766@princeton.edu>
2022-11-14 15:12:27 -0500

    Updated UI spec for bitklavierimage migration

--- spec/system/bitklavierimage_form_submission_spec.rb
+++ spec/system/bitklavierimage_form_submission_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe "Form submission for migrating bitklavierimage", type: :system, m
   let(:user) { FactoryBot.create(:research_data_moderator) }
   let(:title) { "bitKlavier Grand Sample Library—Piano Bar Mic Image" }
   let(:description) do
-    "The bitKlavier Grand consists of sample collections of a new Steinway D grand piano from nine different stereo mic images, with: 16 velocity layers, at every minor 3rd (starting at A0); Hammer release samples; Release resonance samples; Pedal samples. 
+    "The bitKlavier Grand consists of sample collections of a new Steinway D grand piano from nine different stereo mic images, with: 16 velocity layers, at every minor 3rd (starting at A0); Hammer release samples; Release resonance samples; Pedal samples.
 Release packages at 96k/24bit, 88.2k/24bit, 48k/24bit, 44.1k/16bit are available for various applications.
 
-Piano Bar: Earthworks—omni-directionals. 
-This microphone system suspends omnidirectional microphones within the piano. The bar is placed across the harp near the hammers and provides a low string / high string player’s perspective. It also produces a close sound without room or lid interactions. It can be panned across an artificial stereophonic perspective effectively in post-production. 
+Piano Bar: Earthworks—omni-directionals.
+This microphone system suspends omnidirectional microphones within the piano. The bar is placed across the harp near the hammers and provides a low string / high string player’s perspective. It also produces a close sound without room or lid interactions. It can be panned across an artificial stereophonic perspective effectively in post-production.
 File Naming Convention: C4 = middle C. Main note names: [note name][octave]v[velocity].wav -- e.g., “D#5v13.wav”. Release resonance notes: harm[note name][octave]v[velocity].wav -- e.g., “harmC2v2.wav”. Hammer samples: rel[1-88].wav (one per key) -- e.g., “rel23.wav”. Pedal samples: pedal[D/U][velocity].wav -- e.g., “pedalU2.wav” => pedal release (U = up), velocity = 2 (quicker release than velocity = 1).
 This dataset is too large to download directly from this item page. You can access and download the data via Globus (See https://www.youtube.com/watch?v=uf2c7Y1fiFs for instructions on how to use Globus)."
   end
