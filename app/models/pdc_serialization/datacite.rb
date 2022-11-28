7a141bc7d2e680a358a6def43e9a67ac53a28e29
Tyler Wade <tw8766@princeton.edu>
2022-11-28 15:59:21 -0500

    Cleanup

--- app/models/pdc_serialization/datacite.rb
+++ app/models/pdc_serialization/datacite.rb
@@ -276,7 +276,6 @@ module PDCSerialization
         end
 
         def extract_related_objects(resource)
-          byebug
           related_objects = []
           resource.related_objects.each do |ro|
             related_objects << ::Datacite::Mapping::RelatedIdentifier.new(
