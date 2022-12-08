988b3f5ec4fefee0b80fc4b3b215eead5077a797
mccalluc <mccalluc@users.noreply.github.com>
2022-12-08 15:29:48 -0500

    If activestorage is using the filesystem, host needs to be set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
+  include ActiveStorage::SetCurrent
+
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
