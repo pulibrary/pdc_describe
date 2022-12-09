87f69fcbb59ee860de2a29f85a5d21ad65ef705a
Chuck McCallum <mccalluc@users.noreply.github.com>
2022-12-09 15:57:05 -0500

    Fix URI bug when editting a work in a local development environment (#689)
    * If activestorage is using the filesystem, host needs to be set
    
    * explain ActiveStorage::SetCurrent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
+  # This is necessary only for localhost development, with storage configured for the filesystem,
+  # but it shouldn't cause problems in other environments that use S3.
+  # Including this concern lets the disk service generate URLs using
+  # the same host, protocol, and port as the current request.
+  include ActiveStorage::SetCurrent
+
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
