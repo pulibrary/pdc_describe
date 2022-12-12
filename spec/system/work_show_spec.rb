d35b70697294e48d05343d24f8335b68aa450ecb
Hector Correa <hector_correa@princeton.edu>
2022-12-12 12:49:14 -0500

    Added test

--- spec/system/work_show_spec.rb
+++ spec/system/work_show_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe "Creating and updating works", type: :system, js: true do
   let(:user) { work.created_by_user }
 
   before do
+    Capybara.default_max_wait_time = 10
     stub_s3
     page.driver.browser.manage.window.resize_to(2000, 2000)
   end
@@ -15,5 +16,13 @@ RSpec.describe "Creating and updating works", type: :system, js: true do
     visit work_path(work)
     expect(page).to have_content "IS_CITED_BY"
     expect(page).to have_content "https://www.biorxiv.org/content/10.1101/545517v1"
+  end
+
+  it "copies DOI to the clipboard" do
+    sign_in user
+    visit work_path(work)
+    expect(page).to have_content "COPY"
+    find("button#copy-doi").click
+    expect(page).to have_content "COPIED"
   end
 end
