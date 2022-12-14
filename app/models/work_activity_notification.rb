46528dc6e5af684958acd4d9c2cb2559040d6b19
Carolyn Cole <cac9@princeton.edu>
2022-12-14 16:26:09 -0500

    Changing mailing to happen inline so that it will send For some reason if you set the message to a local variable you get an error like `You've accessed the message before...` changing it to a one liner allows the messages to send refs #687

--- app/models/work_activity_notification.rb
+++ app/models/work_activity_notification.rb
@@ -10,8 +10,13 @@ class WorkActivityNotification < ApplicationRecord
       if work.collection&.messages_enabled_for?(user: user)
 
         mailer = NotificationMailer.with(user: user, work_activity: work_activity)
-        message = mailer.build_message
-        message.deliver_later
+
+        #####
+        # Do not separate build_message and deliver_later on separate lines
+        #  You will get an error like `You've accessed the message before...` in production/staging
+        #  I spent a long time trying to replicate the error in a test, but could not
+        mailer.build_message.deliver_later
+        #####
       end
     end
   end
