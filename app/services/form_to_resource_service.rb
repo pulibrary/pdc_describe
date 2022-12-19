b6afee82c421487a2d6be78e49eb943335cdb62e
Chuck McCallum <mccalluc@users.noreply.github.com>
2022-12-19 08:26:52 -0500

    Cleanup form_to_resource_service (#765)
    * make the process_* methods consistent
    
    * unneeded return
    
    * missing param
    
    * Make creators and contributors consistent
    
    * make relate_objects consistent
    
    * revert last
    
    * reorder for consitency
    
    * same pattern with related_objects
    
    * handle titles the same way
    
    * both params are named

--- app/services/form_to_resource_service.rb
+++ app/services/form_to_resource_service.rb
@@ -8,8 +8,8 @@ class FormToResourceService
     #
     # @return [PDCMetadata::Resource] Fully formed resource containing updates from the user
     def convert(params, work)
-      resource = process_curator_controlled(params: params, work: work)
-      resource = process_related_objects(params, resource)
+      resource = reset_resource_to_work(work)
+
       resource.description = params["description"]
       resource.publisher = params["publisher"] if params["publisher"].present?
       resource.publication_year = params["publication_year"] if params["publication_year"].present?
@@ -19,30 +19,16 @@ class FormToResourceService
       resource.award_number = params["award_number"]
       resource.award_uri = params["award_uri"]
 
-      # Process the titles
-      resource = process_titles(params, resource)
-
-      # Process the creators
-      resource = process_creators(params, resource)
-
-      # Process contributors
-      resource = process_contributors(params, resource)
+      process_curator_controlled(params, resource)
+      process_titles(params, resource)
+      process_related_objects(params, resource)
+      process_creators(params, resource)
+      process_contributors(params, resource)
 
       resource
     end
 
     private
-
-      def process_curator_controlled(params:, work:)
-        resource = reset_resource_to_work(work)
-        resource.doi = params["doi"] if params["doi"].present?
-        resource.ark = params["ark"] if params["ark"].present?
-        resource.version_number = params["version_number"] if params["version_number"].present?
-        resource.collection_tags = params["collection_tags"].split(",").map(&:strip) if params["collection_tags"]
-        resource.resource_type = params["resource_type"] if params["resource_type"]
-        resource.resource_type_general = params["resource_type_general"]&.to_sym if params["resource_type_general"]
-        resource
-      end
 
       def reset_resource_to_work(work)
         resource = PDCMetadata::Resource.new
@@ -53,60 +39,80 @@ class FormToResourceService
         resource
       end
 
+      def process_curator_controlled(params, resource)
+        resource.doi = params["doi"] if params["doi"].present?
+        resource.ark = params["ark"] if params["ark"].present?
+        resource.version_number = params["version_number"] if params["version_number"].present?
+        resource.collection_tags = params["collection_tags"].split(",").map(&:strip) if params["collection_tags"]
+        resource.resource_type = params["resource_type"] if params["resource_type"]
+        resource.resource_type_general = params["resource_type_general"]&.to_sym if params["resource_type_general"]
+      end
+
+      # Titles:
+
       def process_titles(params, resource)
         resource.titles << PDCMetadata::Title.new(title: params["title_main"])
-        (1..params["existing_title_count"].to_i).each do |i|
-          if params["title_#{i}"].present?
-            resource.titles << PDCMetadata::Title.new(title: params["title_#{i}"], title_type: params["title_type_#{i}"])
-          end
-        end
-
-        (1..params["new_title_count"].to_i).each do |i|
-          if params["new_title_#{i}"].present?
-            resource.titles << PDCMetadata::Title.new(title: params["new_title_#{i}"], title_type: params["new_title_type_#{i}"])
-          end
-        end
-        resource
+        resource.titles.concat((1..params["existing_title_count"].to_i).filter_map do |i|
+          title = params["title_#{i}"]
+          title_type = params["title_type_#{i}"]
+          new_title(title, title_type)
+        end)
+        resource.titles.concat((1..params["new_title_count"].to_i).filter_map do |i|
+          title = params["new_title_#{i}"]
+          title_type = params["new_title_type_#{i}"]
+          new_title(title, title_type)
+        end)
       end
+
+      def new_title(title, title_type)
+        return if title.blank?
+        PDCMetadata::Title.new(title: title, title_type: title_type)
+      end
+
+      # Related Objects:
+
+      def process_related_objects(params, resource)
+        resource.related_objects = (1..params["related_object_count"].to_i).filter_map do |i|
+          related_identifier = params["related_identifier_#{i}"]
+          related_identifier_type = params["related_identifier_type_#{i}"]
+          relation_type = params["relation_type_#{i}"]
+          new_related_object(related_identifier, related_identifier_type, relation_type)
+        end
+      end
+
+      def new_related_object(related_identifier, related_identifier_type, relation_type)
+        return if related_identifier.blank? && related_identifier_type.blank? && relation_type.blank?
+        PDCMetadata::RelatedObject.new(related_identifier: related_identifier, related_identifier_type: related_identifier_type, relation_type: relation_type)
+      end
+
+      # Creators:
 
       def process_creators(params, resource)
-        (1..params["creator_count"].to_i).each do |i|
-          creator = new_creator(params["given_name_#{i}"], params["family_name_#{i}"], params["orcid_#{i}"], params["sequence_#{i}"])
-          resource.creators << creator unless creator.nil?
+        resource.creators = (1..params["creator_count"].to_i).filter_map do |i|
+          given_name = params["given_name_#{i}"]
+          family_name = params["family_name_#{i}"]
+          orcid = params["orcid_#{i}"]
+          sequence = params["sequence_#{i}"]
+          new_creator(given_name, family_name, orcid, sequence)
         end
-        resource
-      end
-
-      ## TODO: Do the right thing with blank form entries
-      def process_related_objects(params, resource)
-        (1..params["related_object_count"].to_i).each do |i|
-          next if params["related_identifier_#{i}"].blank? && params["related_identifier_type_#{i}"].blank? # do not store blank related identifiers
-          related_object = PDCMetadata::RelatedObject.new(
-                            related_identifier: params["related_identifier_#{i}"],
-                            related_identifier_type: params["related_identifier_type_#{i}"],
-                            relation_type: params["relation_type_#{i}"]
-                          )
-          resource.related_objects << related_object
-        end
-        resource
-      end
-
-      def process_contributors(params, resource)
-        (1..params["contributor_count"].to_i).each do |i|
-          given_name = params["contributor_given_name_#{i}"]
-          family_name = params["contributor_family_name_#{i}"]
-          orcid = params["contributor_orcid_#{i}"]
-          type = params["contributor_role_#{i}"]
-          sequence = params["contributor_sequence_#{i}"]
-          contributor = new_contributor(given_name, family_name, orcid, type, sequence)
-          resource.contributors << contributor unless contributor.nil?
-        end
-        resource
       end
 
       def new_creator(given_name, family_name, orcid, sequence)
         return if family_name.blank? && given_name.blank? && orcid.blank?
         PDCMetadata::Creator.new_person(given_name, family_name, orcid, sequence)
+      end
+
+      # Contributors:
+
+      def process_contributors(params, resource)
+        resource.contributors = (1..params["contributor_count"].to_i).filter_map do |i|
+          given_name = params["contributor_given_name_#{i}"]
+          family_name = params["contributor_family_name_#{i}"]
+          orcid = params["contributor_orcid_#{i}"]
+          type = params["contributor_role_#{i}"]
+          sequence = params["contributor_sequence_#{i}"]
+          new_contributor(given_name, family_name, orcid, type, sequence)
+        end
       end
 
       def new_contributor(given_name, family_name, orcid, type, sequence)
