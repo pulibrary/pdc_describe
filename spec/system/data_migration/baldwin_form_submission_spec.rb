d7f55d759db4805b30e2036ceea4e76110e9a77d
Hector Correa <hector_correa@princeton.edu>
2023-01-05 16:48:58 -0500

    More of less automates the process to export data from spec/system/data_migration and import it into a new instance via a rake task

--- spec/system/data_migration/baldwin_form_submission_spec.rb
+++ spec/system/data_migration/baldwin_form_submission_spec.rb
@@ -64,6 +64,8 @@ A full description of the structure of the dataset and how to reproduce the figu
       # This will allow us to evolve our local datacite standards and test our records against them.
       datacite = PDCSerialization::Datacite.new_from_work(baldwin_work)
       expect(datacite.valid?).to eq true
+
+      export_spec_data("baldwin.json", baldwin_work.to_json)
     end
   end
 end
