4fada87b85641b3d0b3723902108bc9592fc37a7
Hector Correa <hector@hectorcorrea.com>
2023-01-09 09:59:08 -0500

    More of less automates the process to export data from spec/system/data_migration and import it into a new instance via a rake task (#822)

--- spec/system/data_migration/cklibrary_form_submission_spec.rb
+++ spec/system/data_migration/cklibrary_form_submission_spec.rb
@@ -56,6 +56,8 @@ RSpec.describe "Form submission for migrating cklibrary", type: :system, mock_ez
       # This will allow us to evolve our local datacite standards and test our records against them.
       datacite = PDCSerialization::Datacite.new_from_work(cklibrary_work)
       expect(datacite.valid?).to eq true
+
+      export_spec_data("cklibrary.json", cklibrary_work.to_json)
     end
   end
 end
