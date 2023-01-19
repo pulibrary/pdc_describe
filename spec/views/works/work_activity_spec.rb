9fbbd9c0f074c3b7ebb4e8547bccc4b8b57f6f09
mccalluc <mccalluc@users.noreply.github.com>
2023-01-19 15:06:08 -0500

    stub more tests

--- spec/views/works/work_activity_spec.rb
+++ spec/views/works/work_activity_spec.rb
@@ -3,28 +3,41 @@ require "rails_helper"
 
 describe "Change History, AKA Provenance" do
   let(:work) { FactoryBot.create :draft_work }
-  let(:partial) { "works/work_activity_provenance.html.erb" }
+  let(:partial) { "works/work_activity_provenance" }
 
-  it "renders" do
-    assign(:work, work)
+  it "handles no activity" do
     assign(:changes, [])
 
     render(partial: partial, locals: { can_add_provenance_note: false })
 
     expect(rendered).to include("No activity")
   end
+
+  it "shows oldest message first, when array is in the same order" do
+
+  end
+
+  it "shows oldest message first, when array is in the reverse order" do
+
+  end 
 end
 
 describe "Messages" do
-  let(:work) { FactoryBot.create :draft_work }
-  let(:partial) { "works/work_activity_messages.html.erb" }
+  let(:work) { FactoryBot.create :draft_work } # Needed only to check work.submission_notes.
+  let(:partial) { "works/work_activity_messages" }
 
-  it "renders" do
+  it "handles no messages" do
     assign(:work, work)
     assign(:messages, [])
-
     render(partial: partial)
-
     expect(rendered).to include("No messages")
   end
+
+  it "shows newest message first, when array is in the same order" do
+
+  end
+
+  it "shows newest message first, when array is in the reverse order" do
+
+  end  
 end
