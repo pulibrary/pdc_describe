f5a9b3c379e546a3a1df5f528a9efcc6a523602f
mccalluc <mccalluc@users.noreply.github.com>
2023-01-23 15:33:23 -0500

    parameterize the two tests

--- spec/models/work_state_spec.rb
+++ spec/models/work_state_spec.rb
@@ -8,25 +8,21 @@ RSpec.describe "Work state transions", type: :model do
     none_work: :draft!,
     draft_work: :complete_submission!
   }.each do |fixture, method_sym|
-    context "a #{fixture}" do
-      let(:work) { FactoryBot.create(fixture) }
-
-      it "Creates work activity notifications for the curator & the creator after #{method_sym}" do
-        curator_user # make sure the curator exists
-        user = work.created_by_user
-        expect do
-          work.send(method_sym, user)
-        end.to change { WorkActivity.count }.by(2)
-          .and change { WorkActivityNotification.count }.by(2)
-      end
-
-      it "Creates a single work activity notification for the curator = creator after #{method_sym}" do
-        work.created_by_user.add_role(:collection_admin, work.collection)
-        user = work.created_by_user
-        expect do
-          work.send(method_sym, user)
-        end.to change { WorkActivity.count }.by(2)
-          .and change { WorkActivityNotification.count }.by(1)
+    [true, false].each do |creator_is_admin|
+      context "a #{fixture} and creator #{creator_is_admin ? 'is' : 'not'} admin" do
+        let(:work) { FactoryBot.create(fixture) }
+        it "Creates work activity notifications for the curator & the creator after #{method_sym}" do
+          user = work.created_by_user
+          if creator_is_admin
+            user.add_role(:collection_admin, work.collection)  
+          else
+            curator_user # make sure the curator exists
+          end
+          expect do
+            work.send(method_sym, user)
+          end.to change { WorkActivity.count }.by(2)
+            .and change { WorkActivityNotification.count }.by(creator_is_admin ? 1 : 2)
+        end
       end
     end
   end
