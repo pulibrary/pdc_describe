7c86c7d510027b937813b22bd4f597b3e1d06b11
mccalluc <mccalluc@users.noreply.github.com>
2023-01-23 14:49:20 -0500

    refactor test as loop

--- spec/models/work_state_spec.rb
+++ spec/models/work_state_spec.rb
@@ -4,39 +4,22 @@ require "rails_helper"
 RSpec.describe "Work state transions", type: :model do
   let(:curator_user) { FactoryBot.create :user, collections_to_admin: [work.collection] }
 
-  context "a none work" do
-    let(:work) { FactoryBot.create(:none_work) }
+  {
+    none_work: :draft!,
+    draft_work: :complete_submission!,
+    awaiting_approval_work: :approve!
+  }.each do |fixture, method_sym|
+    context "a #{fixture}" do
+      let(:work) { FactoryBot.create(fixture) }
 
-    it "Creates a work activity notification for the curator & the user when drafted" do
-      curator_user # make sure the curator exists
-      expect do
-        work.draft!(work.created_by_user)
-      end.to change { WorkActivity.count }.by(2)
-        .and change { WorkActivityNotification.count }.by(2)
+      it "Creates a work activity notification for the curator & the user after #{method_sym}" do
+        curator_user # make sure the curator exists
+        allow(work).to receive(:publish) # Only relevant for approve.
+        expect do
+          work.send(method_sym, method_sym == :approve! ? curator_user : work.created_by_user)
+        end.to change { WorkActivity.count }.by(2)
+          .and change { WorkActivityNotification.count }.by(2)
+      end
     end
-  end
-
-  context "a draft work" do
-    let(:work) { FactoryBot.create(:draft_work) }
-
-    it "Creates a work activity notification for the curator & the user when completed" do
-      curator_user # make sure the curator exists
-      expect do
-        work.complete_submission!(work.created_by_user)
-      end.to change { WorkActivity.count }.by(2)
-         .and change { WorkActivityNotification.count }.by(2)
-    end
-  end
-
-  context "a completed work" do
-    let(:work) { FactoryBot.create(:awaiting_approval_work) }
-
-    it "Creates a work activity notification for the curator & the user when approved" do
-      allow(work).to receive(:publish)
-      expect do
-        work.approve!(curator_user)
-      end.to change { WorkActivity.count }.by(2)
-         .and change { WorkActivityNotification.count }.by(2)
-    end
-  end
+  end  
 end
