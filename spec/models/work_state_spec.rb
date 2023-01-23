1fc5f7bac169e97637d2ba49c5cc433cce5f631d
mccalluc <mccalluc@users.noreply.github.com>
2023-01-23 13:48:34 -0500

    Union uses; rearrange tests for consistency

--- spec/models/work_state_spec.rb
+++ spec/models/work_state_spec.rb
@@ -2,16 +2,18 @@
 require "rails_helper"
 
 RSpec.describe "Work state transions", type: :model do
-  let(:work) { FactoryBot.create(:none_work) }
-
   let(:curator_user) { FactoryBot.create :user, collections_to_admin: [work.collection] }
 
-  it "Creates a work activity notification for the curator & the user when drafted" do
-    curator_user # make sure the curator exists
-    expect do
-      work.draft!(work.created_by_user)
-    end.to change { WorkActivity.count }.by(2)
-       .and change { WorkActivityNotification.count }.by(2)
+  context "a none work" do
+    let(:work) { FactoryBot.create(:none_work) }
+
+    it "Creates a work activity notification for the curator & the user when drafted" do
+      curator_user # make sure the curator exists
+      expect do
+        work.draft!(work.created_by_user)
+      end.to change { WorkActivity.count }.by(2)
+        .and change { WorkActivityNotification.count }.by(2)
+    end
   end
 
   context "a draft work" do
