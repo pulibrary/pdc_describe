3d300a43b6d1db2c08716738b113d1080a3d9c8c
mccalluc <mccalluc@users.noreply.github.com>
2023-01-26 16:38:32 -0500

    update html form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,7 +31,7 @@ module ApplicationHelper
 
   # rubocop:disable Rails/OutputSafety
   # rubocop:disable Metrics/MethodLength
-  def contributor_link(contributor, add_separator)
+  def individual_contributor_link(contributor, add_separator)
     return if contributor.value.blank?
 
     icon_html = ""
@@ -44,6 +44,39 @@ module ApplicationHelper
     if contributor.orcid.present?
       icon_html = '<img alt="ORCID logo" src="https://info.orcid.org/wp-content/uploads/2019/11/orcid_16x16.png" width="16" height="16" />'
       name_html = '<a href="https://orcid.org/' + contributor.orcid + '" target="_blank">' + display_name + "</a>" + separator
+    else
+      name_html = display_name + separator
+    end
+
+    html = <<-HTML
+      <span class="author-name">
+        #{icon_html}
+        #{name_html}
+      </span>
+    HTML
+    html.html_safe
+  end
+  # rubocop:enable Rails/OutputSafety
+  # rubocop:enable Metrics/MethodLength
+
+  # rubocop:disable Rails/OutputSafety
+  # rubocop:disable Metrics/MethodLength
+  def organizational_contributor_link(contributor, add_separator)
+    # TODO: Copy-and-paste. Filed an issue to clean it all up:
+    # https://github.com/pulibrary/pdc_describe/issues/906
+    return if contributor.value.blank?
+
+    icon_html = ""
+    separator = add_separator ? ";" : ""
+    display_name = if contributor.type.present?
+                     "#{contributor.value} (#{contributor.type.titleize})"
+                   else
+                     contributor.value
+                   end
+    if contributor.ror.present?
+      # Logo from https://ror.readme.io/docs/ror-id-display-guidelines-and-logos
+      icon_html = '<img alt="ROR logo" src="https://raw.githubusercontent.com/ror-community/ror-logos/main/ror-icon-rgb.svg" height="24" />'
+      name_html = '<a href="https://ror.org/' + contributor.ror + '" target="_blank">' + display_name + "</a>" + separator
     else
       name_html = display_name + separator
     end
