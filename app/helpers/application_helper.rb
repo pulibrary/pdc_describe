70f304f02ac87a929c7032136bba7fcdcd67153b
mccalluc <mccalluc@users.noreply.github.com>
2023-01-27 15:26:13 -0500

    one helper works in both places

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,28 +8,6 @@ module ApplicationHelper
   end
 
   # rubocop:disable Rails/OutputSafety
-  def person_orcid_link(name, orcid, add_separator)
-    return if name.blank?
-
-    icon_html = '<i class="bi bi-person-fill"></i>'
-    separator = add_separator ? ";" : ""
-    name_html = "#{name}#{separator}"
-    if orcid.present?
-      icon_html = '<img alt="ORCID logo" src="https://info.orcid.org/wp-content/uploads/2019/11/orcid_16x16.png" width="16" height="16" />'
-      name_html = '<a href="https://orcid.org/' + orcid + '" target="_blank">' + name + "</a>" + separator
-    end
-
-    html = <<-HTML
-    <span class="author-name">
-      #{icon_html}
-      #{name_html}
-    </span>
-    HTML
-    html.html_safe
-  end
-  # rubocop:enable Rails/OutputSafety
-
-  # rubocop:disable Rails/OutputSafety
   # rubocop:disable Metrics/MethodLength
   def contributor_link(contributor, add_separator)
     return if contributor.value.blank?
