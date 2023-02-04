062f9e5b27a58cea0dde7e6c899f437c4a30132f
mccalluc <mccalluc@users.noreply.github.com>
2023-02-04 11:49:54 -0500

    --fix

--- app/javascript/entrypoints/pdc/maximum_file_upload.js
+++ app/javascript/entrypoints/pdc/maximum_file_upload.js
@@ -1,24 +1,23 @@
 export default class MaximumFileUpload {
+  constructor(upload_id, save_id, error_id = 'file-error') {
+    this.upload_element = $(`#${upload_id}`);
+    this.save_element = document.getElementById(save_id);
+    this.error_element = document.getElementById(error_id);
+    this.attach_validation();
+  }
 
-    constructor(upload_id, save_id, error_id = "file-error") {
-      this.upload_element = $("#"+upload_id);
-      this.save_element = document.getElementById(save_id);
-      this.error_element = document.getElementById(error_id)
-      this.attach_validation();
-    }
-  
-    attach_validation() {
-        console.log("attaching validation")
-        this.upload_element.on("change", this.validate.bind(this));
-    }
+  attach_validation() {
+    console.log('attaching validation');
+    this.upload_element.on('change', this.validate.bind(this));
+  }
 
-    validate() {
-        if (this.upload_element[0].files.length > 20) {
-            this.save_element.disabled = true;
-            this.error_element.innerText = "You can select a maximum of 20 files";
-        } else {
-            this.save_element.disabled = false;
-            this.error_element.innerText = "";
-        }
+  validate() {
+    if (this.upload_element[0].files.length > 20) {
+      this.save_element.disabled = true;
+      this.error_element.innerText = 'You can select a maximum of 20 files';
+    } else {
+      this.save_element.disabled = false;
+      this.error_element.innerText = '';
     }
-}  
+  }
+}
--- app/javascript/entrypoints/pdc/pdc_ui_loader.js
+++ app/javascript/entrypoints/pdc/pdc_ui_loader.js
@@ -1,12 +1,12 @@
-import MaximumFileUpload from './maximum_file_upload.js'
+import MaximumFileUpload from './maximum_file_upload.js';
 
 export default class PdcUiLoader {
   run() {
-    this.setup_fileupload_validation()
+    this.setup_fileupload_validation();
   }
 
   setup_fileupload_validation() {
-    new MaximumFileUpload("patch_pre_curation_uploads","file-upload");
-    new MaximumFileUpload("pre_curation_uploads","btn-submit");
+    new MaximumFileUpload('patch_pre_curation_uploads', 'file-upload');
+    new MaximumFileUpload('pre_curation_uploads', 'btn-submit');
   }
 }
