18a8f956096d3be652bd42a0c6e1671b4c029693
carolyncole <1599081+carolyncole@users.noreply.github.com>
2023-02-06 08:22:44 -0600

    Allowing vite javascript to not be minifed in the Development environment (#949)
    Grabbed this technique from Jeff Knox here: https://www.jeffreyknox.dev/blog/dropping-webpack-for-vite-part-2/

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 
-export default defineConfig({
-  plugins: [
-    RubyPlugin(),
-  ],
-})
+export default ({ command, mode }) => {
+  let minifySetting
+
+  if (mode === "development") {
+    minifySetting = false
+  } else {
+    minifySetting = "esbuild" 
+  }
+
+  return {
+    build: {
+      minify: minifySetting
+    },
+    plugins: [
+      RubyPlugin(),
+    ],
+  }
+}
