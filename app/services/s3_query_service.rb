2155547ad43842478614b8b9c68854bfe4bf1dd2
carolyncole <1599081+carolyncole@users.noreply.github.com>
2023-04-19 15:17:15 -0400

    Updating pagination of file listing to allow for dspace bucket and prefix (#1080)

--- app/services/s3_query_service.rb
+++ app/services/s3_query_service.rb
@@ -125,7 +125,7 @@ class S3QueryService
       resp = client.list_objects_v2({ bucket: bucket_name, max_keys: 1000, prefix: prefix })
       resp_hash = resp.to_h
       objects = parse_objects(resp_hash)
-      objects += parse_continuation(resp_hash)
+      objects += parse_continuation(resp_hash, bucket_name: bucket_name, prefix: prefix)
       elapsed = Time.zone.now - start
       Rails.logger.info("Loading S3 objects. Bucket: #{bucket_name}. Prefix: #{prefix}. Elapsed: #{elapsed} seconds")
       objects
@@ -232,7 +232,7 @@ class S3QueryService
       objects
     end
 
-    def parse_continuation(resp_hash)
+    def parse_continuation(resp_hash, bucket_name: self.bucket_name, prefix: self.prefix)
       objects = []
       while resp_hash[:is_truncated]
         token = resp_hash[:next_continuation_token]
