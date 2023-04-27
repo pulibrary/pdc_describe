fdf8b97097829c32a70bcf80d11b90fa3f9b0004
jrgriffiniii <1443986+jrgriffiniii@users.noreply.github.com>
2023-04-27 16:42:26 -0400

    wip

---
+++ spec/support/simple_cov_helper.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "active_support/inflector"
+require "simplecov"
+
+class SimpleCovHelper
+  def self.report_coverage(base_dir: "./coverage_results")
+    SimpleCov.configure do
+      minimum_coverage(100)
+    end
+    new(base_dir: base_dir).merge_results
+  end
+
+  attr_reader :base_dir
+
+  def initialize(base_dir:)
+    @base_dir = base_dir
+  end
+
+  def all_results
+    Dir["#{base_dir}/.resultset*.json"]
+  end
+
+  def merge_results
+    SimpleCov.collate all_results
+  end
+end
+
+SimpleCovHelper.report_coverage
