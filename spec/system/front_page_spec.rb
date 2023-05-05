c4d339c4de2da08706b158ef9e0fc14c9c11ee00
Kate Lynch <katelynch@princeton.edu>
2023-05-05 16:25:26 -0400

    Rubocop fixes

--- spec/system/front_page_spec.rb
+++ spec/system/front_page_spec.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
-require 'rails_helper'
+require "rails_helper"
 
-describe 'Application landing page', type: :system do
-    it "has a footer with latest deploy information" do
-      visit '/'
-      expect(page).to have_content "last updated"
-    end
-  
-    it "has a header with links to helpful info" do
-      visit '/'
-      expect(page).to have_link "About", href: "/about"
-    end
+describe "Application landing page", type: :system do
+  it "has a footer with latest deploy information" do
+    visit "/"
+    expect(page).to have_content "last updated"
   end
+
+  it "has a header with links to helpful info" do
+    visit "/"
+    expect(page).to have_link "About", href: "/about"
+  end
+end
--- spec/views/welcome/about.html.erb_spec.rb
+++ spec/views/welcome/about.html.erb_spec.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
-require 'rails_helper'
+require "rails_helper"
 
-describe '/about', type: :system do
-    it 'renders the about page' do
-      visit '/about'
-      expect(page).to have_text("About Princeton's PDC Research Data Repository")
-    end
+describe "/about", type: :system do
+  it "renders the about page" do
+    visit "/about"
+    expect(page).to have_text("About Princeton's PDC Research Data Repository")
   end
+end
