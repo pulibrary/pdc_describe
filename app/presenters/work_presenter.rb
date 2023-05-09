bfe48474ca91ba5698a4712781bb3f24a42438cf
Bess Sadler <bess.sadler@princeton.edu>
2023-05-09 17:06:16 -0400

    Link related objects (or not) as indicated by the kind of object it is

--- app/presenters/work_presenter.rb
+++ app/presenters/work_presenter.rb
@@ -13,4 +13,28 @@ class WorkPresenter
     return if value.nil?
     Rinku.auto_link(value, :all, 'target="_blank"')
   end
+
+  def related_objects_link_list
+    ro = resource.related_objects
+    ro.map { |a| format_related_object_links(a) }
+  end
+
+  # relation_type, identifier, link
+  def format_related_object_links(related_object)
+    rol = RelatedObjectLink.new
+    rol.identifier = related_object.related_identifier
+    rol.relation_type = related_object.relation_type
+    rol.link = format_link(related_object.related_identifier, related_object.related_identifier_type)
+    rol
+  end
+
+  # Turn an identifier into a link. This will vary for different kinds of related objects.
+  # A DOI URL is not the same as an arXiv URL, for example.
+  # For now, only format links for DOIs.
+  def format_link(id, id_type)
+    return id if id.starts_with?("http")
+    return "https://doi.org/#{id}" if id_type == "DOI"
+    return "https://arxiv.org/abs/#{id}" if id_type == "arXiv"
+    ""
+  end
 end
--- spec/presenters/work_presenter_spec.rb
+++ spec/presenters/work_presenter_spec.rb
@@ -14,4 +14,35 @@ RSpec.describe WorkPresenter do
       expect(work_presenter.description).to eq("This tests the link <a href=\"http://library.princeton.edu\" target=\"_blank\">http://library.princeton.edu</a>. It also has a summary.")
     end
   end
+
+  describe "#related_objects" do
+    let(:related_doi) { FactoryBot.build(:related_object) }
+    let(:related_arxiv) { FactoryBot.build(:related_object_arxiv) }
+    let(:related_isbn) { FactoryBot.build(:related_object_isbn) }
+
+    before do
+      work.resource.related_objects << related_doi
+      work.resource.related_objects << related_arxiv
+      work.resource.related_objects << related_isbn
+      work.save
+    end
+
+    it "formats identifiers in related objects as URLs so they can be links" do
+      ro1 = work_presenter.related_objects_link_list[0]
+      expect(ro1).to be_instance_of RelatedObjectLink
+      expect(ro1.identifier).to eq related_doi.related_identifier
+      expect(ro1.relation_type).to eq "IsCitedBy"
+      expect(ro1.link).to eq "https://doi.org/#{related_doi.related_identifier}"
+
+      ro2 = work_presenter.related_objects_link_list[1]
+      expect(ro2.identifier).to eq related_arxiv.related_identifier
+      expect(ro2.relation_type).to eq "IsCitedBy"
+      expect(ro2.link).to eq "https://arxiv.org/abs/#{related_arxiv.related_identifier}"
+
+      ro3 = work_presenter.related_objects_link_list[2]
+      expect(ro3.identifier).to eq related_isbn.related_identifier
+      expect(ro3.relation_type).to eq "IsCitedBy"
+      expect(ro3.link).to eq ""
+    end
+  end
 end
