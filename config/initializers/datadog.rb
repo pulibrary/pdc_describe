e7faa8dc7d3fc8f04f572e5df69ef19d2312857c
Bess Sadler <bess.sadler@princeton.edu>
2023-05-12 17:16:04 -0400

    Configure datadog some more

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 Datadog.configure do |c|
+  c.service = "pdc-describe"
+  c.tracing.report_hostname = true
+  c.tracing.analytics.enabled = true
+  c.tracing.enabled = true
+  c.tracing.report_hostname = true
   # Add additional configuration here.
   # Activate integrations, change tracer settings, etc...
 end
