b5d6c0caef2988a86a57fecac8b9c0a92aeb1e65
Bess Sadler <bess.sadler@princeton.edu>
2023-05-15 12:06:31 -0400

    Update datadog config

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,10 +1,28 @@
 # frozen_string_literal: true
-Datadog.configure do |c|
-  c.service = "pdc-describe"
-  c.tracing.report_hostname = true
-  c.tracing.analytics.enabled = true
-  c.tracing.enabled = true
-  c.tracing.report_hostname = true
-  # Add additional configuration here.
-  # Activate integrations, change tracer settings, etc...
+
+if Rails.env.staging? || Rails.env.production?
+  Datadog.configure do |c|
+    c.service = "pdc-describe"
+    c.tracing.report_hostname = true
+    c.tracing.analytics.enabled = true
+    c.tracing.enabled = true
+    c.tracing.report_hostname = true
+
+    # Rails
+    c.use :rails
+
+    # Redis
+    c.use :redis
+
+    # Net::HTTP
+    c.use :http
+
+    # Sidekiq
+    c.use :sidekiq
+
+    # Faraday
+    c.use :faraday
+    # Add additional configuration here.
+    # Activate integrations, change tracer settings, etc...
+  end
 end
