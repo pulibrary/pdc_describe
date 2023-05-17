a2a4839622c904598abaa3df018c670adcee6f64
Bess Sadler <bess.sadler@princeton.edu>
2023-05-17 16:26:54 -0400

    Eliminate annoying DataDog warnings in development and test environments (#1186)
    * Format logs for DataDog analysis
    
    * Style fix

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,39 +1,38 @@
 # frozen_string_literal: true
 
-if Rails.env.staging? || Rails.env.production?
-  require "datadog/statsd"
-  require "ddtrace"
+require "datadog/statsd"
+require "ddtrace"
 
-  Datadog.configure do |c|
-    c.service = "pdc-describe"
-    c.tracing.report_hostname = true
-    c.tracing.analytics.enabled = true
-    c.tracing.enabled = true
-    c.tracing.report_hostname = true
+Datadog.configure do |c|
+  c.env = Rails.env
+  c.service = "pdc-describe"
+  c.tracing.report_hostname = true
+  c.tracing.analytics.enabled = true
+  c.tracing.enabled = Rails.env.staging? || Rails.env.production?
+  c.tracing.report_hostname = true
 
-    # From https://docs.datadoghq.com/tracing/metrics/runtime_metrics/ruby/
-    # To enable runtime metrics collection, set `true`. Defaults to `false`
-    # You can also set DD_RUNTIME_METRICS_ENABLED=true to configure this.
-    c.runtime_metrics.enabled = true
+  # From https://docs.datadoghq.com/tracing/metrics/runtime_metrics/ruby/
+  # To enable runtime metrics collection, set `true`. Defaults to `false`
+  # You can also set DD_RUNTIME_METRICS_ENABLED=true to configure this.
+  c.runtime_metrics.enabled = true
 
-    # Optionally, you can configure the DogStatsD instance used for sending runtime metrics.
-    # DogStatsD is automatically configured with default settings if `dogstatsd-ruby` is available.
-    # You can configure with host and port of Datadog agent; defaults to 'localhost:8125'.
-    c.runtime_metrics.statsd = Datadog::Statsd.new
+  # Optionally, you can configure the DogStatsD instance used for sending runtime metrics.
+  # DogStatsD is automatically configured with default settings if `dogstatsd-ruby` is available.
+  # You can configure with host and port of Datadog agent; defaults to 'localhost:8125'.
+  c.runtime_metrics.statsd = Datadog::Statsd.new
 
-    # Rails
-    c.tracing.instrument :rails
+  # Rails
+  c.tracing.instrument :rails
 
-    # Redis
-    c.tracing.instrument :redis
+  # Redis
+  c.tracing.instrument :redis
 
-    # Net::HTTP
-    c.tracing.instrument :http
+  # Net::HTTP
+  c.tracing.instrument :http
 
-    # Sidekiq
-    c.tracing.instrument :sidekiq
+  # Sidekiq
+  c.tracing.instrument :sidekiq
 
-    # Faraday
-    c.tracing.instrument :faraday
-  end
+  # Faraday
+  c.tracing.instrument :faraday
 end
