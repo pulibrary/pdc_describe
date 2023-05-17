4d8237608edffcf351a57a6e8122b601e4d89d23
Bess Sadler <bess.sadler@princeton.edu>
2023-05-17 16:05:07 -0400

    Format logs for DataDog analysis

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require_relative "lando_env"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module DrdsRailsTemplate
+module PdcDescribe
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
