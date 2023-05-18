dd590aaf25e8874a72c46b2bdae71edbaac3277d
Bess Sadler <bess.sadler@princeton.edu>
2023-05-18 13:04:40 -0400

    Remove Datadog::Statsd

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -14,12 +14,12 @@ Datadog.configure do |c|
   # From https://docs.datadoghq.com/tracing/metrics/runtime_metrics/ruby/
   # To enable runtime metrics collection, set `true`. Defaults to `false`
   # You can also set DD_RUNTIME_METRICS_ENABLED=true to configure this.
-  c.runtime_metrics.enabled = true
+  # c.runtime_metrics.enabled = true
 
   # Optionally, you can configure the DogStatsD instance used for sending runtime metrics.
   # DogStatsD is automatically configured with default settings if `dogstatsd-ruby` is available.
   # You can configure with host and port of Datadog agent; defaults to 'localhost:8125'.
-  c.runtime_metrics.statsd = Datadog::Statsd.new
+  # c.runtime_metrics.statsd = Datadog::Statsd.new
 
   # Rails
   c.tracing.instrument :rails
@@ -28,10 +28,10 @@ Datadog.configure do |c|
   # c.tracing.instrument :redis
 
   # Net::HTTP
-  # c.tracing.instrument :http
+  c.tracing.instrument :http
 
   # Sidekiq
-  c.tracing.instrument :sidekiq
+  # c.tracing.instrument :sidekiq
 
   # Faraday
   c.tracing.instrument :faraday
