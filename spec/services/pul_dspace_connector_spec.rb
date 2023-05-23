1a2b5e36bfe6404a1814ab1ff557749eec4d6ca6
carolyncole <1599081+carolyncole@users.noreply.github.com>
2023-05-23 10:37:44 -0400

    Adding a better error message when bitstreams do not pass checksum test (#1203)
    refs #1146

--- spec/services/pul_dspace_connector_spec.rb
+++ spec/services/pul_dspace_connector_spec.rb
@@ -30,4 +30,45 @@ RSpec.describe PULDspaceConnector, type: :model do
       expect(dspace_data.doi).to be_empty
     end
   end
+
+  context "the work is a dspace migrated object" do
+    let(:work) { FactoryBot.create :shakespeare_and_company_work }
+    let(:handle_body) { File.read(Rails.root.join("spec", "fixtures", "files", "dspace_handle.json")) }
+    let(:bitsreams_body) { File.read(Rails.root.join("spec", "fixtures", "files", "dspace_bitstreams_response.json")) }
+    let(:metadata_body) { File.read(Rails.root.join("spec", "fixtures", "files", "dspace_metadata_response.json")) }
+    let(:bitsream1_body) { File.read(Rails.root.join("spec", "fixtures", "files", "bitstreams", "SCoData_combined_v1_2020-07_README.txt")) }
+    let(:bitsream2_body) { File.read(Rails.root.join("spec", "fixtures", "files", "bitstreams", "SCoData_combined_v1_2020-07_datapackage.json")) }
+    let(:bitsream3_body) { File.read(Rails.root.join("spec", "fixtures", "files", "bitstreams", "license.txt")) }
+    before do
+      stub_request(:get, "https://dataspace.example.com/rest/handle/88435/dsp01zc77st047")
+        .to_return(status: 200, body: handle_body, headers: {})
+      stub_request(:get, "https://dataspace.example.com/rest/items/104718/bitstreams")
+        .to_return(status: 200, body: bitsreams_body, headers: {})
+      stub_request(:get, "https://dataspace.example.com/rest/items/104718/metadata")
+        .to_return(status: 200, body: metadata_body, headers: {})
+      stub_request(:get, "https://dataspace.example.com/rest//bitstreams/145784/retrieve")
+        .to_return(status: 200, body: bitsream1_body, headers: {})
+      stub_request(:get, "https://dataspace.example.com/rest//bitstreams/145785/retrieve")
+        .to_return(status: 200, body: bitsream2_body, headers: {})
+      stub_request(:get, "https://dataspace.example.com/rest//bitstreams/145762/retrieve")
+        .to_return(status: 200, body: bitsream3_body, headers: {})
+    end
+
+    it "downloads the bitstreams" do
+      allow(Honeybadger).to receive(:notify)
+      expect(dspace_data.download_bitstreams.count).to eq(3)
+      expect(Honeybadger).not_to have_received(:notify)
+    end
+
+    context "a bitstream missmatch" do
+      # realy should be the readme, but we are intetionally returning the wrong data
+      let(:bitsream1_body) { "not the readme!!" }
+
+      it "downloads the bitstreams" do
+        allow(Honeybadger).to receive(:notify)
+        expect(dspace_data.download_bitstreams.count).to eq(3)
+        expect(Honeybadger).to have_received(:notify).with(/Mismatching checksum .* for work: #{work.id} doi: #{work.doi} ark: #{work.ark}/)
+      end
+    end
+  end
 end
