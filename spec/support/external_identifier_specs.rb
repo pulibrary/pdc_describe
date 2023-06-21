ef4b16bd3dc29c2e26862e87fa9f1ffc0a8e79f4
Hector Correa <hector_correa@princeton.edu>
2023-06-21 16:20:12 -0400

    Updated the test to mock the response as JSON (rather than JSONP)

--- spec/support/external_identifier_specs.rb
+++ spec/support/external_identifier_specs.rb
@@ -82,7 +82,11 @@ class FakeIdentifierIntegration < Sinatra::Base
   # rubocop:enable Metrics/MethodLength
 
   get "/orcid/:orcid" do |orcid|
-    content_type(:js)
+
+    # Notice that we force "application/json" on the JavaScript call and therefore
+    # we must use `content_type :json` in our response as well.
+    content_type(:json)
+
     callback = params[:callback]
     data = {
       "orcid-identifier" => {
@@ -107,7 +111,7 @@ class FakeIdentifierIntegration < Sinatra::Base
       data["person"]["name"]["family-name"]["value"] = "Loya"
     end
 
-    "#{callback}(#{data.to_json});"
+    data.to_json
   end
 end
 
