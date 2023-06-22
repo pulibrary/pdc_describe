59a644c622f949b9862861c2f63a3b936b299c51
Hector Correa <hector_correa@princeton.edu>
2023-06-22 12:33:28 -0400

    Handle nil family names

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.all.sort_by(&:family_name)
+    @users = User.all.sort_by { |user| user.family_name || "" }
   end
 
   # GET /users/1
