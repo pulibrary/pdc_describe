be7e226f828fcec2c5cfa76fa27961906253bfb1
carolyncole <1599081+carolyncole@users.noreply.github.com>
2023-07-19 13:58:31 -0400

    Move the WorkActivity::MIGRATION_START to before the dspace migration so that it can be completed successfully if there are no globus files (#1340)
    fixes #1324

--- app/controllers/work_migration_controller.rb
+++ app/controllers/work_migration_controller.rb
@@ -20,12 +20,8 @@ class WorkMigrationController < ApplicationController
   private
 
     def run_migration(work)
-      dspace = PULDspaceMigrate.new(work)
+      dspace = PULDspaceMigrate.new(work, current_user)
       dspace.migrate
       flash[:notice] = dspace.migration_message
-      WorkActivity.add_work_activity(work.id, { migration_id: dspace.migration_snapshot.id,
-                                                message: dspace.migration_message, file_count: dspace.file_keys.count,
-                                                directory_count: dspace.directory_keys.count }.to_json,
-                                     current_user.id, activity_type: WorkActivity::MIGRATION_START)
     end
 end
