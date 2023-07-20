7374d097829be1d31f79d966673ac5872afa6183
Hector Correa <hector@hectorcorrea.com>
2023-07-20 12:37:52 -0400

    Handles snapshots with no files (#1357)

--- db/migrate/20230719173219_add_type_to_upload_snapshot.rb
+++ db/migrate/20230719173219_add_type_to_upload_snapshot.rb
@@ -2,7 +2,7 @@ class AddTypeToUploadSnapshot < ActiveRecord::Migration[6.1]
   def up
     add_column :upload_snapshots, :type, 'string'
     UploadSnapshot.all.each do |upload_snapshot|
-      if upload_snapshot.files.first["migrate_status"].blank?
+      if upload_snapshot.files.count == 0 || upload_snapshot.files.first["migrate_status"].blank?
         upload_snapshot.type = "UploadSnapshot"
       else
         upload_snapshot.type = "MigrationUploadSnapshot"
