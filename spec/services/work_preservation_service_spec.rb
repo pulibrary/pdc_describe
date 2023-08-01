a1ce6ec8de2f7c50233a77ef1617467ba8d9f839
Hector Correa <hector@hectorcorrea.com>
2023-08-01 11:30:16 -0400

    Stores preservation files in an S3 bucket with a different availability region (#1387)
    * First pass at storing the preservation files in an S3 bucket in a different region from our pre and post curation S3 buckets
    
    * Allow to configure the bucket name in s3_stub since now we have code that switches the bucket. Updated on test to use it this new value.
    
    * Moved the approval tests to its own section so that we can test that it uses a different bucket
    
    * Updated the test to check that the preservation service was called (instead of looking at the details of the preservation)
    
    * Minor tweaks
    
    * Oops
    
    * Switched the S3QueryService class to use a string mode parameter (instead of two booleans)

--- spec/services/work_preservation_service_spec.rb
+++ spec/services/work_preservation_service_spec.rb
@@ -4,7 +4,6 @@ require "rails_helper"
 RSpec.describe WorkPreservationService do
   describe "preserve in S3" do
     let(:approved_work) { FactoryBot.create :approved_work, doi: "10.34770/pe9w-x904" }
-    let(:bucket_name) { approved_work.s3_query_service.bucket_name }
     let(:path) { approved_work.s3_query_service.prefix }
     let(:preservation_directory) { path + "/princeton_data_commons/" }
     let(:file1) { FactoryBot.build :s3_file, filename: "#{approved_work.doi}/#{approved_work.id}/anyfile1.txt", last_modified: Time.parse("2022-04-21T18:29:40.000Z") }
@@ -19,16 +18,16 @@ RSpec.describe WorkPreservationService do
     let(:files) { [file1, file2, preservation_file1] }
 
     before do
-      stub_s3(data: files)
+      stub_s3(data: files, bucket_name: "example-bucket-preservation")
     end
 
     it "preserves a work to the indicated location in S3" do
-      subject = described_class.new(work_id: approved_work.id, bucket_name: bucket_name, path: path)
-      expect(subject.preserve!).to eq "s3://example-bucket/#{preservation_directory}"
+      subject = described_class.new(work_id: approved_work.id, path: path)
+      expect(subject.preserve!).to eq "s3://example-bucket-preservation/#{preservation_directory}"
     end
 
     it "excludes the preservation files from the preservation metadata" do
-      subject = described_class.new(work_id: approved_work.id, bucket_name: bucket_name, path: path)
+      subject = described_class.new(work_id: approved_work.id, path: path)
       metadata = JSON.parse(subject.preservation_metadata)
       expect(metadata["files"].any? { |file| file["filename"] == file1.filename }).to be true
       expect(metadata["files"].any? { |file| file["filename"] == file2.filename }).to be true
@@ -46,7 +45,7 @@ RSpec.describe WorkPreservationService do
     end
 
     it "preserves a work locally" do
-      subject = described_class.new(work_id: approved_work.id, bucket_name: "localhost", path: local_path)
+      subject = described_class.new(work_id: approved_work.id, path: local_path, localhost: true)
       location = subject.preserve!
       expect(location.start_with?("file:///")).to be true
       expect(location.end_with?("#{local_path}/princeton_data_commons/")).to be true
--- spec/support/s3_query_service_specs.rb
+++ spec/support/s3_query_service_specs.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-def stub_s3(data: [], bucket_url: nil, prefix: "10.34770/123-abc/1")
+def stub_s3(data: [], bucket_url: nil, prefix: "10.34770/123-abc/1", bucket_name: "example-bucket")
   @s3_client = instance_double(Aws::S3::Client)
   allow(@s3_client).to receive(:head_object)
   allow(@s3_client).to receive(:delete_object)
@@ -8,7 +8,7 @@ def stub_s3(data: [], bucket_url: nil, prefix: "10.34770/123-abc/1")
 
   fake_s3_query = instance_double(S3QueryService, data_profile: { objects: data, ok: true }, client: @s3_client,
                                                   client_s3_files: data, prefix: prefix)
-  mock_methods(fake_s3_query, data)
+  mock_methods(fake_s3_query, data, bucket_name)
   allow(S3QueryService).to receive(:new).and_return(fake_s3_query)
 
   mock_bucket(bucket_url)
@@ -16,8 +16,8 @@ def stub_s3(data: [], bucket_url: nil, prefix: "10.34770/123-abc/1")
   fake_s3_query
 end
 
-def mock_methods(fake_s3_query, data)
-  allow(fake_s3_query).to receive(:bucket_name).and_return("example-bucket")
+def mock_methods(fake_s3_query, data, bucket_name)
+  allow(fake_s3_query).to receive(:bucket_name).and_return(bucket_name)
   allow(fake_s3_query).to receive(:file_count).and_return(data.length)
   allow(fake_s3_query).to receive(:delete_s3_object)
   allow(fake_s3_query).to receive(:create_directory)
