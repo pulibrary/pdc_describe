6f976f526ca32839c9ffec20d9a6fb67a729609f
Carolyn Cole <cac9@princeton.edu>
2023-08-03 07:48:28 -0400

    Make direct messages always send emails Make sure that direct message always send emails even if the user has emails disabled fixes #1382

--- app/models/work_activity_notification.rb
+++ app/models/work_activity_notification.rb
@@ -5,9 +5,9 @@ class WorkActivityNotification < ApplicationRecord
   belongs_to :user
 
   after_create do
-    if user.email_messages_enabled?
+    if user.email_messages_enabled? || direct_message?
       work = work_activity.work
-      if work.group&.messages_enabled_for?(user: user)
+      if work.group&.messages_enabled_for?(user: user) || direct_message?
 
         mailer = NotificationMailer.with(user: user, work_activity: work_activity)
         message = mailer.build_message
@@ -15,4 +15,10 @@ class WorkActivityNotification < ApplicationRecord
       end
     end
   end
+
+  private
+
+    def direct_message?
+      @direct_message ||= work_activity.activity_type == WorkActivity::MESSAGE && work_activity.message.include?("@#{user.uid}")
+    end
 end
