9907140bc9604d07b2779a8d34970a1dbd6b22b6
James R. Griffin III <1443986+jrgriffiniii@users.noreply.github.com>
2023-08-30 12:04:40 -0400

    Ensuring that the ApplicationHelper test coverage is increased (#1488)
    * Ensuring that the ApplicationHelper test coverage is increased; Excluding coverage analysis for the file lib/simplecov_helper.rb
    
    * wip
    
    * wip
    
    * wip
    
    * wip

--- spec/system/work_spec.rb
+++ spec/system/work_spec.rb
@@ -248,6 +248,9 @@ RSpec.describe "Creating and updating works", type: :system do
       expect(contributor_text_after).to eq("Simon Gallup  Robert Smith")
       click_on "Save Work"
       draft_work.reload
+
+      expect(draft_work.resource).not_to be nil
+      expect(draft_work.resource.individual_contributors).not_to be_empty
       expect(draft_work.resource.individual_contributors.last.given_name).to eq("Robert")
       expect(draft_work.resource.individual_contributors.last.family_name).to eq("Smith")
     end
