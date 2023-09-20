7a190366ac4a86123f39de1624391522edebf826
Bess Sadler <bess.sadler@princeton.edu>
2023-09-20 16:22:45 -0400

    WIP: service to make renamed files manifest file

---
+++ app/services/file_rename_mapping_service.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+# We sometimes have data with filenames that contain characters that AWS S3 cannot handle. In those cases we want to:
+# 1.  Rename the files to something that is AWS legal. Replace all illegal characters with a _ (underscore)
+# 2.  Ensure there are no duplicate file names after the renaming by appending a (1), (2) at the end of the filename
+#     if the file has been renamed
+# 3.  Keep a record of all of the file names as they originally existed and what they were renamed to
+# 4.  The record goes into a file called files_renamed.txt, which contains a list of all files that have been renamed
+#     and what they were renamed to, along with a timestamp
+# 5.  This files_renamed.txt file gets added to the dataset as a payload file, akin to a README.txt or license.txt
+class FileRenameMappingService
+  attr_reader :upload_snapshot, :files
+
+  def initialize(upload_snapshot:)
+    @upload_snapshot = upload_snapshot
+    @files = parse_files_to_rename
+  end
+
+  def original_filenames
+    @upload_snapshot.files.map { |a| a["filename"] }
+  end
+
+  def parse_files_to_rename
+    files = []
+    original_filenames.each do |original_filename|
+      files << FileRenameService.new(filename: original_filename)
+    end
+    files
+  end
+
+  # A rename is needed if any of the original filenames need renaming
+  def rename_needed?
+    @files.each do |file|
+      return true if file.needs_rename?
+    end
+    false
+  end
+
+  # Format: "Sep 19 2023"
+  def rename_date
+    Time.zone.now.strftime("%d %b %Y")
+  end
+
+  def renaming_document
+    message = "Some files have been renamed to comply with AWS S3 storage requirements\n"
+    message += "Rename date: #{rename_date}\n"
+    message += "Original Filename\t Renamed File\n"
+    @files.each do |file|
+      message += "#{file.original_filename}\t#{file.new_filename}\n"
+    end
+    message
+  end
+end
