bfad39b29f483c061c2dda29f0053e9696c20b28
Carolyn Cole <cac9@princeton.edu>
2023-09-21 09:52:55 -0400

    Implemented test for any user to not have the provenance form
    Co-authored-by: Favour Nwogbo <favournwogbo21@users.noreply.github.com>

--- spec/system/provenance_note_spec.rb
+++ spec/system/provenance_note_spec.rb
@@ -2,19 +2,23 @@
 require "rails_helper"
 
 RSpec.describe "Adding a Provenance note", type: :system, js: true do
-  let(:work) { FactoryBot.create(:draft_work, created_by_user_id: user) }
 
   context "A user that does not have permissions to edit the provenance" do
-    let(:user) { FactoryBot.create(:user) }
+    let(:any_user) { FactoryBot.create(:user) }
+    let(:work) { FactoryBot.create(:draft_work, created_by_user_id: any_user.id) }
     it "does not show the form" do
-        login_as user
-      pending "This should test that the page does not have the form"
+      login_as any_user
+      visit work_path(work)
+      expect(page).to have_content(work.title)
+      expect(page).not_to have_button("Add Provenance Note")
     end
   end
 
   context "A user that does has permissions to edit the provenance" do
-    let(:user) { FactoryBot.create(:user, uid: config.provenance_message_writers.first) }
+    let(:provenace_writer_user) { FactoryBot.create(:user, uid: Rails.configuration.provenance_message_writers.first) }
+    let(:work) { FactoryBot.create(:draft_work, created_by_user_id: provenace_writer_user.id) }
     it "does show the form" do
+      login_as provenace_writer_user
       pending "This should test that the page does have the form"
     end
   end
