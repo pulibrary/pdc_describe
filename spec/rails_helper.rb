dcc398dcfcb1b9877b968cad86d3d913c1f3448c
carolyncole <1599081+carolyncole@users.noreply.github.com>
2023-10-11 16:17:56 -0400

    Move Simplecov to the top of the file to make it calculate the coverage correctly (#1571)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,22 +1,6 @@
 # frozen_string_literal: true
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require "simplecov"
-require "spec_helper"
-ENV["RAILS_ENV"] ||= "test"
-require File.expand_path("../config/environment", __dir__)
-# Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
-# Add additional requires below this line. Rails is not loaded until this point!
-
-require "rspec/rails"
-require "axe-rspec"
-require "database_cleaner/active_record"
-require "ffaker"
-
-# NOTE: require 'devise' after require 'rspec/rails'
-require "devise"
-
-require "simplecov"
 require "simplecov_json_formatter"
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
   [
@@ -36,6 +20,21 @@ SimpleCov.start "rails" do
   # I am uncertain as to why this is not being included in the code coverage analysis report, but the coverage cannot be increased even with explicit testing
   add_filter "app/helpers/application_helper.rb"
 end
+
+require "spec_helper"
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../config/environment", __dir__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+# Add additional requires below this line. Rails is not loaded until this point!
+
+require "rspec/rails"
+require "axe-rspec"
+require "database_cleaner/active_record"
+require "ffaker"
+
+# NOTE: require 'devise' after require 'rspec/rails'
+require "devise"
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
