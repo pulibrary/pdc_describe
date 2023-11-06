4bd241ac537a93da81ccd49a4c6ce0c76b5a8670
Carolyn Cole <cac9@princeton.edu>
2023-11-06 08:45:22 -0500

    Allowing the revision line to be a roll back

--- spec/system/front_page_spec.rb
+++ spec/system/front_page_spec.rb
@@ -12,4 +12,15 @@ describe "Application landing page", type: :system do
     expect(page).to have_link "About", href: /about/
     expect(page).to have_link "How to Submit", href: /how-to-submit/
   end
+
+  context "an error in the footer" do
+    before do
+      allow(VersionFooter).to receive(:info).and_return({ error: "Error!!!" })
+    end
+
+    it "has a footer with error information" do
+      visit "/"
+      expect(page).to have_content "Error!!!"
+    end
+  end
 end
