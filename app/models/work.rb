3dec05cb270b2d9f6d5ddb59a230efb7e97e46c6
carolyncole <1599081+carolyncole@users.noreply.github.com>
2023-12-04 08:15:31 -0500

    Adding datacite validation to valid_to_submit so users know any errors in the metadata before submitting to the curators (#1620)
    * Moving work validation to a Service before we make it more complex
    
    * Adding datacite validation to valid_to_submit so users know any errors in the metadata before submitting to the curators

--- app/models/work.rb
+++ app/models/work.rb
@@ -20,6 +20,8 @@ class Work < ApplicationRecord
 
   alias state_history user_work
 
+  delegate :valid_to_submit, :valid_to_draft, :valid_to_approve, to: :work_validator
+
   include AASM
 
   aasm column: :state do
@@ -121,14 +123,8 @@ class Work < ApplicationRecord
     end
   end
 
-  validate do |work|
-    if none?
-      work.validate_ids
-    elsif draft?
-      work.valid_to_draft
-    else
-      work.valid_to_submit
-    end
+  validate do |_work|
+    work_validator.valid?
   end
 
   # Overload ActiveRecord.reload method
@@ -147,60 +143,6 @@ class Work < ApplicationRecord
     # Force `resource` to be reloaded
     @resource = nil
     self
-  end
-
-  def validate_doi
-    return true unless user_entered_doi
-    return false unless unique_doi
-    if /^10.\d{4,9}\/[-._;()\/:a-z0-9\-]+$/.match?(doi.downcase)
-      response = Faraday.get("#{Rails.configuration.datacite.doi_url}#{doi}")
-      errors.add(:base, "Invalid DOI: can not verify it's authenticity") unless response.success? || response.status == 302
-    else
-      errors.add(:base, "Invalid DOI: does not match format")
-    end
-    errors.count == 0
-  end
-
-  def unique_doi
-    other_record = Work.find_by_doi(doi)
-    return true if other_record == self
-    errors.add(:base, "Invalid DOI: It has already been applied to another work #{other_record.id}")
-    false
-  rescue ActiveRecord::RecordNotFound
-    true
-  end
-
-  def validate_ids
-    validate_doi
-    unique_ark
-  end
-
-  def valid_to_draft
-    errors.add(:base, "Must provide a title") if resource.main_title.blank?
-    validate_ark
-    validate_creators
-    validate_related_objects if resource.present? && !resource.related_objects.empty?
-    errors.count == 0
-  end
-
-  def valid_to_submit
-    valid_to_draft
-    validate_metadata
-    errors.count == 0
-  end
-
-  def valid_to_approve(user)
-    valid_to_submit
-    if resource.doi.blank?
-      errors.add :base, "DOI must be present for a work to be approved"
-    end
-    unless user.has_role? :group_admin, group
-      errors.add :base, "Unauthorized to Approve"
-    end
-    if pre_curation_uploads_fast.empty? && post_curation_uploads.empty?
-      errors.add :base, "Uploads must be present for a work to be approved"
-    end
-    errors.count == 0
   end
 
   def title
@@ -530,32 +472,11 @@ class Work < ApplicationRecord
     embargo_date >= current_date
   end
 
-  # rubocop:disable Metrics/AbcSize
-  def validate_metadata
-    return if metadata.blank?
-    validate_required_metadata
-    errors.add(:base, "Must provide a description") if resource.description.blank?
-    errors.add(:base, "Must indicate the Publisher") if resource.publisher.blank?
-    errors.add(:base, "Must indicate the Publication Year") if resource.publication_year.blank?
-    errors.add(:base, "Must indicate at least one Rights statement") if resource.rights_many.count == 0
-    errors.add(:base, "Must provide a Version number") if resource.version_number.blank?
-    validate_related_objects
-  end
-  # rubocop:enable Metrics/AbcSize
-
-  def validate_related_objects
-    return if resource.related_objects.empty?
-    invalid = resource.related_objects.reject(&:valid?)
-    if invalid.count.positive?
-      related_object_errors = "Related Objects are invalid: "
-      prev_errors = errors.to_a
-      prev_related_object_errors = prev_errors.map { |e| e.include?(related_object_errors) }.reduce(:|)
-
-      errors.add(:base, "#{related_object_errors}#{invalid.map(&:errors).join(', ')}") unless prev_related_object_errors
-    end
-  end
-
   protected
+
+    def work_validator
+      @work_validator ||= WorkValidator.new(self)
+    end
 
     # This must be protected, NOT private for ActiveRecord to work properly with this attribute.
     #   Protected will still keep others from setting the metatdata, but allows ActiveRecord the access it needs
@@ -615,44 +536,6 @@ class Work < ApplicationRecord
       uw.save!
       WorkActivity.add_work_activity(id, "marked as #{state.to_s.titleize}", user.id, activity_type: WorkActivity::SYSTEM)
       WorkStateTransitionNotification.new(self, user.id).send
-    end
-
-    def validate_ark
-      return if ark.blank?
-      return false unless unique_ark
-      first_save = id.blank?
-      changed_value = metadata["ark"] != ark
-      if first_save || changed_value
-        errors.add(:base, "Invalid ARK provided for the Work: #{ark}") unless Ark.valid?(ark)
-      end
-    end
-
-    def unique_ark
-      return true if ark.blank?
-      other_record = Work.find_by_ark(ark)
-      return true if other_record == self
-      errors.add(:base, "Invalid ARK: It has already been applied to another work #{other_record.id}")
-      false
-    rescue ActiveRecord::RecordNotFound
-      true
-    end
-
-    def validate_required_metadata
-      return if metadata.blank?
-      errors.add(:base, "Must provide a title") if resource.main_title.blank?
-      validate_creators
-    end
-
-    def validate_creators
-      if resource.creators.count == 0
-        errors.add(:base, "Must provide at least one Creator")
-      else
-        resource.creators.each do |creator|
-          if creator.orcid.present? && Orcid.invalid?(creator.orcid)
-            errors.add(:base, "ORCID for creator #{creator.value} is not in format 0000-0000-0000-0000")
-          end
-        end
-      end
     end
 
     # This needs to be called #before_save
--- spec/services/work_validator_spec.rb
+++ spec/services/work_validator_spec.rb
@@ -2,7 +2,7 @@
 require "rails_helper"
 
 RSpec.describe Work, type: :model do
-  describe "#validate_ark" do
+  describe "#valid_to_draft" do
     let(:work) { FactoryBot.build(:draft_work, ark: "ark:/88435/dsp01zc77st047") }
     before do
       allow(Ark).to receive(:valid?).and_return(true)
@@ -41,6 +41,22 @@ RSpec.describe Work, type: :model do
         expect(work.save).to be_truthy
         expect(Ark).not_to have_received(:valid?)
       end
+    end
+  end
+
+  context "datacite xml is invalid" do
+    let(:work) do
+      work = FactoryBot.create :draft_work
+      work.resource.individual_contributors = [PDCMetadata::Creator.new_individual_contributor("Sally", "Smith", "", "", 0)]
+      work.save
+      work
+    end
+
+    it "is not valid" do
+      validator = WorkValidator.new(work)
+      expect(validator.valid?).to be_truthy # we can still save, we just can not transition to submitted
+      expect(validator.valid_to_submit).to be_falsey
+      expect(work.errors.full_messages).to eq(["Contributor: Type cannot be nil"])
     end
   end
 end
