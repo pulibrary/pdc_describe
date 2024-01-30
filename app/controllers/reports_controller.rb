bd9ad1b274f65b472db27733fceac11fd9c3e789
Claudia Lee <claudialee57@gmail.com>
2024-01-30 14:01:10 -0500

    displays data

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 class ReportsController < ApplicationController
     def index
-        
+        @works = Work.all
+        #@works[0]["metadata"]["publication_year"]
+        #@works[0].title
+        #@works[0].group.title
+        #@works[0].total_file_size
     end
     
 end
