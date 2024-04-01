d28d6811204ace121685b560111fa92b5b33b76b
Carolyn Cole <cac9@princeton.edu>
2024-04-01 14:24:51 -0400

    Make sure the wizard buttons are a mirror image in forward and reverse

--- spec/system/work_wizard_reverse_spec.rb
+++ spec/system/work_wizard_reverse_spec.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
 require "rails_helper"
 
-describe "walk the wizard in reverse", type: :system do
-  let(:user) { FactoryBot.create(:princeton_submitter) }
+describe "walk the wizard in reverse", type: :system, js: true do
 
-  it "goes to the correct pages" do
-    work = FactoryBot.create :draft_work
-    sign_in user
+  it "follow the same path in reverse or forward" do
+    work = FactoryBot.create :draft_work, files_location: "file_upload"
+    stub_s3 data: [FactoryBot.build(:s3_readme, work:)]
+    sign_in work.created_by_user
     visit work_review_path(work)
 
     expect(page).to have_content "Data curators will review"
+    click_on "Go Back"
+
+    expect(page).to have_content "Once you have uploaded"
     click_on "Go Back"
 
     expect(page).to have_content "Begin the process to upload your submission"
@@ -22,10 +25,13 @@ describe "walk the wizard in reverse", type: :system do
     click_on "Save Work"
 
     expect(page).to have_content "Please upload the README"
-    click_on "Save Work"
+    click_on "Continue"
 
     expect(page).to have_content "Begin the process to upload your submission"
-    click_on "Save Work"
+    click_on "Continue"
+
+    expect(page).to have_content "Once you have uploaded"
+    click_on "Continue"
 
     expect(page).to have_content "Data curators will review"
   end
