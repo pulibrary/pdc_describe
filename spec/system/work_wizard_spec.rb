f7e58dc9d05611294f138d74a40167cbe9c33586
Carolyn Cole <cac9@princeton.edu>
2024-04-02 14:30:53 -0400

    Add previous and next for file_upload and review inclind adding it to the tests Also changed the spacing to make each page more visible

--- spec/system/work_wizard_spec.rb
+++ spec/system/work_wizard_spec.rb
@@ -20,52 +20,65 @@ describe "walk the wizard hitting all the buttons", type: :system, js: true do
     readme_form_css = "form[action='/works/#{work.id}/readme-uploaded']"
     upload_form_css = "form[action='/works/#{work.id}/attachment-select']"
     file_upload_form_css = "form[action='/works/#{work.id}/file-upload']"
+    validate_form_css = "form[action='/works/#{work.id}/validate']"
 
     expect(page).to have_css(new_submission_form_css)
     click_on "Next"
-
+    
     expect(page).to have_css(edit_form_css)
-
     click_on "Previous"
     expect(page).to have_css(new_submission_form_css)
-
     click_on "Next"
     expect(page).to have_css(edit_form_css)
-
     fill_in "description", with: "description"
-
     expect { click_on "Save" }.to change { work.work_activity.count }.by(1)
     expect(page).to have_css(edit_form_css)
-
     click_on "Next"
     expect(page).to have_css(readme_form_css)
     expect(page).not_to have_content("previously uploaded")
 
     click_on "Previous"
     expect(page).to have_css(edit_form_css)
-
     click_on "Next"
     expect(page).to have_css(readme_form_css)
-
     stub_s3 data: [FactoryBot.build(:s3_readme, work:)]
     click_on "Save"
     expect(page).to have_css(readme_form_css)
     expect(page).to have_content("previously uploaded")
-
     click_on "Next"
     expect(page).to have_css(upload_form_css)
 
     click_on "Previous"
     expect(page).to have_css(readme_form_css)
-
     click_on "Next"
+    expect(work.reload.files_location).to be_nil
     expect(page).to have_css(upload_form_css)
-
+    page.find(:xpath, "//input[@value='file_upload']").choose
     click_on "Save"
-    sleep(1)
+    expect(work.reload.files_location). to eq("file_upload")
     expect(page).to have_css(upload_form_css)
-
     click_on "Next"
     expect(page).to have_css(file_upload_form_css)
+
+    click_on "Previous"
+    expect(page).to have_css(upload_form_css)
+    click_on "Next"
+    expect(page).to have_css(file_upload_form_css)
+    click_on "Save"
+    sleep(1) # no actual upload occured so no real change to be seen
+    expect(page).to have_css(file_upload_form_css)
+    click_on "Next"
+    expect(page).to have_css(validate_form_css)
+
+    click_on "Previous"
+    expect(page).to have_css(file_upload_form_css)
+    click_on "Next"
+    expect(page).to have_css(validate_form_css)
+    fill_in "submission_notes", with: "notes"
+    expect { click_on "Save" }.to change { work.work_activity.count }.by(1)
+    expect(page).to have_css(validate_form_css)
+    click_on "Grant License and Complete"
+    expect(page).to have_content("Welcome")
+    expect(page).to have_content(work.title)
   end
 end
