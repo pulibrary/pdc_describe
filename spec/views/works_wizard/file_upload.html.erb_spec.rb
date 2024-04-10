10bb6e172e25287a6a86ab3c02b850d25c2cd659
Hector Correa <hector_correa@princeton.edu>
2024-04-10 16:37:48 -0400

    Got this test to pass but I think we should remove it

--- spec/views/works_wizard/file_upload.html.erb_spec.rb
+++ spec/views/works_wizard/file_upload.html.erb_spec.rb
@@ -7,16 +7,21 @@ describe "works_wizard/file_upload.html.erb" do
   before do
     Group.create_defaults
     user
+    file_list_response = File.read(Rails.root.join("spec", "fixtures", "s3_list_bucket_result.xml"))
     stub_datacite(host: "api.datacite.org", body: datacite_register_body(prefix: "10.34770"))
+    stub_request(:get, /https\:\/\/example-bucket.s3.amazonaws.com\/\?list-type/).
+      to_return(status: 200, body: file_list_response, headers: {})
   end
 
   let(:work) { FactoryBot.create :draft_work }
+  let(:work_decorator) { WorkDecorator.new(work, user)}
 
   it "supports multiple file uploads" do
     assign(:work, work)
+    assign(:work_decorator, work_decorator)
 
     render
 
-    expect(rendered).to include("<input multiple=\"multiple\" type=\"file\"")
+    expect(rendered).to include('<div id="file-upload-area">')
   end
 end
