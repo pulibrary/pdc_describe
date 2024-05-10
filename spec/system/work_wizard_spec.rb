9d761496f4014ce1184197c3bd051ee2259d0091
leefaisonr <rl3667@princeton.edu>
2024-05-10 16:20:54 -0400

    Adds step for the user to accept dialogue

--- spec/system/work_wizard_spec.rb
+++ spec/system/work_wizard_spec.rb
@@ -93,6 +93,7 @@ describe "walk the wizard hitting all the buttons", type: :system, js: true do
     expect(page).to have_css(validate_form_css)
     expect(page).to have_content("this is not on the page")
     click_on "Grant License and Complete"
+    page.driver.browser.switch_to.alert.accept
     expect(page).to have_content("Welcome")
     expect(page).to have_content(work.title)
   end
