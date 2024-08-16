581b31b6f9ef8b405195afc9b076bc139754d533
Francis Kayiwa <kayiwa@pobox.com>
2024-08-16 10:10:35 -0400

    add a new production VM

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 server "pdc-describe-prod1.princeton.edu", user: "deploy", roles: %w[app db web]
 server "pdc-describe-prod2.princeton.edu", user: "deploy", roles: %w[app db web]
+server "pdc-describe-prod3.princeton.edu", user: "deploy", roles: %w[app db web]
