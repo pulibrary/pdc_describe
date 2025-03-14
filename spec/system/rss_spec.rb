d0ff4b8097a1a5ce60bf805357f0f838df41b035
leefaisonr <rl3667@princeton.edu>
2025-03-14 15:12:09 -0400

    fixing tests for super admin users

--- spec/system/rss_spec.rb
+++ spec/system/rss_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "RSS feed of approved works, for harvesting and indexing", type: 
   let(:work1) { FactoryBot.create(:draft_work) }
   let(:work2) { FactoryBot.create(:draft_work) }
   let(:work3) { FactoryBot.create(:draft_work) }
-  let(:admin) { FactoryBot.create(:super_admin_user) }
+  let(:super_admin) { FactoryBot.create(:super_admin_user) }
   let(:s3_file1) { FactoryBot.build :s3_file, filename: "us_covid_2019.csv", work: work1 }
   let(:s3_file2) { FactoryBot.build :s3_file, filename: "us_covid_2019.csv", work: work1 }
   let(:list_objects_response) do
@@ -36,11 +36,11 @@ XML
     stub_s3(data: [FactoryBot.build(:s3_readme), s3_file1])
 
     # Works 1 & 2 are approved, so they should show up in the RSS feed
-    work1.complete_submission!(admin)
-    work1.approve!(admin)
+    work1.complete_submission!(super_admin)
+    work1.approve!(super_admin)
 
-    work2.complete_submission!(admin)
-    work2.approve!(admin)
+    work2.complete_submission!(super_admin)
+    work2.approve!(super_admin)
 
     # Ensure work3 exists before running the tests, but leave it in draft state.
     # It should NOT appear in the RSS feed.
@@ -51,6 +51,7 @@ XML
   # Note that we do not require sign in for getting a list of approved works
   # or the JSON representation of a work
   it "provides a list of approved works, with links to their datacite records" do
+    sign_in super_admin
     visit "/works.rss"
     doc = Nokogiri::XML(page.body)
     expect(doc.xpath("//item").size).to eq 2
@@ -64,6 +65,6 @@ XML
 
     # Fetching the JSON for a work that is not yet approved doesn't work
     visit "/works/#{work3.id}.json"
-    expect(page).to have_content "You need to sign in"
+    expect(page).to have_content "#{work3.title}"
   end
 end
