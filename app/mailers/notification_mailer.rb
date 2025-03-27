72827b512c6c0ef94b40c4a91a9008f0a2a58927
HyppoliteJ <jh6441@princeton.edu>
2025-03-27 15:08:41 -0400

    Pulling out common logic into its own method

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -11,12 +11,7 @@ class NotificationMailer < ApplicationMailer
     @message_html = @work_activity.to_html
     @url = work_url(@work_activity.work)
 
-    # Troubleshooting https://github.com/pulibrary/pdc_describe/issues/1783
-    if @url.include?("/describe/describe/")
-      Rails.logger.error("URL #{@url} included /describe/describe/ and was fixed. See https://github.com/pulibrary/pdc_describe/issues/1783")
-      @url = @url.gsub("/describe/describe/", "/describe/")
-    end
-
+    @url = check_url(@url)
     mail(to: @user.email, subject: @subject)
   end
 
@@ -29,12 +24,7 @@ class NotificationMailer < ApplicationMailer
     @message_html = @work_activity.to_html
     @url = work_url(@work_activity.work)
 
-    # Troubleshooting https://github.com/pulibrary/pdc_describe/issues/1783
-    if @url.include?("/describe/describe/")
-      Rails.logger.error("URL #{@url} included /describe/describe/ and was fixed. See https://github.com/pulibrary/pdc_describe/issues/1783")
-      @url = @url.gsub("/describe/describe/", "/describe/")
-    end
-
+    @url = check_url(@url)
     mail(to: @user.email, subject: @subject)
   end
 
@@ -47,12 +37,7 @@ class NotificationMailer < ApplicationMailer
     @message_html = @work_activity.to_html
     @url = work_url(@work_activity.work)
 
-    # Troubleshooting https://github.com/pulibrary/pdc_describe/issues/1783
-    if @url.include?("/describe/describe/")
-      Rails.logger.error("URL #{@url} included /describe/describe/ and was fixed. See https://github.com/pulibrary/pdc_describe/issues/1783")
-      @url = @url.gsub("/describe/describe/", "/describe/")
-    end
-
+    @url = check_url(@url)
     mail(to: @user.email, subject: @subject)
   end
 
@@ -65,12 +50,16 @@ class NotificationMailer < ApplicationMailer
     @message_html = @work_activity.to_html
     @url = work_url(@work_activity.work)
 
-    # Troubleshooting https://github.com/pulibrary/pdc_describe/issues/1783
-    if @url.include?("/describe/describe/")
-      Rails.logger.error("URL #{@url} included /describe/describe/ and was fixed. See https://github.com/pulibrary/pdc_describe/issues/1783")
-      @url = @url.gsub("/describe/describe/", "/describe/")
-    end
-
+    @url = check_url(@url)
     mail(to: @user.email, subject: @subject)
+  end
+
+  def check_url(url)
+    # Troubleshooting https://github.com/pulibrary/pdc_describe/issues/1783
+    if url.include?("/describe/describe/")
+      Rails.logger.error("URL #{url} included /describe/describe/ and was fixed. See https://github.com/pulibrary/pdc_describe/issues/1783")
+      url = url.gsub("/describe/describe/", "/describe/")
+    end
+    url
   end
 end
