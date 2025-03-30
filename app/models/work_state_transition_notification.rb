4d7be78c149d7a6d3a2b29e435a53f604a9443af
HyppoliteJ <jh6441@princeton.edu>
2025-03-30 13:13:54 -0400

    fixing reject message bug

--- app/models/work_state_transition_notification.rb
+++ app/models/work_state_transition_notification.rb
@@ -21,11 +21,11 @@ class WorkStateTransitionNotification
     end
 
     @work_title = work.title
-    @notification = notification_for_transition
     @id = work.id
 
     raise(NotImplementedError, "Invalid user ID provided.") if current_user_id.nil?
     @current_user_id = current_user_id
+    @notification = notification_for_transition
   end
 
   def send
@@ -47,7 +47,7 @@ class WorkStateTransitionNotification
           when :none
             "#{user_tags} [#{work_title}](#{work_url}) has been created."
           when :awaiting_approval
-            "#{depositor.full_name} (#{depositor.id}) at #{Time.now.utc} returned the following submission to you for revision: #{work_title}"
+            "#{user_tags} #{User.find(@current_user_id).full_name} at #{Time.now.utc} returned the following submission to you for revision: #{work_title}"
           end
         end
       end
