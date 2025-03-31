2b75d6b3c74e5091f6aabea08231c65d58ac5739
Jaymee Hyppolite <54775395+JaymeeH@users.noreply.github.com>
2025-03-31 14:47:53 -0400

    Improving messges for curators/moderators (#2066)
    * Adding new submission and review message for curators
    
    * refactoring how aasm states are accessed & adding from_state checks in the work spec
    
    * Pulling out common logic into its own method
    
    * accessing the from state in the work activity notification
    
    * adding a todo note
    
    * fixing reject message bug
    
    * cleaning up leftover comments
    
    * adding comment
    
    * refactor the check from state method
    adding work state spec tests

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -11,12 +11,33 @@ class NotificationMailer < ApplicationMailer
     @message_html = @work_activity.to_html
     @url = work_url(@work_activity.work)
 
-    # Troubleshooting https://github.com/pulibrary/pdc_describe/issues/1783
-    if @url.include?("/describe/describe/")
-      Rails.logger.error("URL #{@url} included /describe/describe/ and was fixed. See https://github.com/pulibrary/pdc_describe/issues/1783")
-      @url = @url.gsub("/describe/describe/", "/describe/")
-    end
+    @url = check_url(@url)
+    mail(to: @user.email, subject: @subject)
+  end
 
+  def new_submission_message
+    @user = params[:user]
+    @work_activity = params[:work_activity]
+
+    @subject = "[pdc-describe] New Submission Created"
+    @message = @work_activity.message
+    @message_html = @work_activity.to_html
+    @url = work_url(@work_activity.work)
+
+    @url = check_url(@url)
+    mail(to: @user.email, subject: @subject)
+  end
+
+  def review_message
+    @user = params[:user]
+    @work_activity = params[:work_activity]
+
+    @subject = "[pdc-describe] Submission Ready for Review"
+    @message = @work_activity.message
+    @message_html = @work_activity.to_html
+    @url = work_url(@work_activity.work)
+
+    @url = check_url(@url)
     mail(to: @user.email, subject: @subject)
   end
 
@@ -29,12 +50,16 @@ class NotificationMailer < ApplicationMailer
     @message_html = @work_activity.to_html
     @url = work_url(@work_activity.work)
 
-    # Troubleshooting https://github.com/pulibrary/pdc_describe/issues/1783
-    if @url.include?("/describe/describe/")
-      Rails.logger.error("URL #{@url} included /describe/describe/ and was fixed. See https://github.com/pulibrary/pdc_describe/issues/1783")
-      @url = @url.gsub("/describe/describe/", "/describe/")
-    end
-
+    @url = check_url(@url)
     mail(to: @user.email, subject: @subject)
+  end
+
+  def check_url(url)
+    # Troubleshooting https://github.com/pulibrary/pdc_describe/issues/1783
+    if url.include?("/describe/describe/")
+      Rails.logger.error("URL #{url} included /describe/describe/ and was fixed. See https://github.com/pulibrary/pdc_describe/issues/1783")
+      url = url.gsub("/describe/describe/", "/describe/")
+    end
+    url
   end
 end
