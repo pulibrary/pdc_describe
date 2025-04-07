bb4fed8994f1a95a619ef5fea1dbcd919b631c50
Hector Correa <hector_correa@princeton.edu>
2025-04-07 15:27:02 -0400

    Documented todo

--- app/models/researcher.rb
+++ app/models/researcher.rb
@@ -14,6 +14,9 @@ class Researcher < ApplicationRecord
 
   def self.autocomplete_list(search_term)
     researchers = []
+    # This way of filtering researchers pulls all of them from PostgreSQL to
+    # Ruby. We will change this to use a WHERE clause to filter directly in
+    # PostgreSQL as part of https://github.com/pulibrary/pdc_describe/issues/2085
     Researcher.all.find_each do |researcher|
       if researcher.match?(search_term)
         display_value = "#{researcher.first_name} #{researcher.last_name} (#{researcher.orcid})"
