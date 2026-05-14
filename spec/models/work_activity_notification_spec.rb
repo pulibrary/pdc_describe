862dbc30795d5550303b30c409def51632ef357f
Carolyn Cole <cac9@princeton.edu>
2026-05-14 09:07:48 -0400

    Updating messaging to make sure user messages are sent separately from system messages

--- spec/models/work_activity_notification_spec.rb
+++ spec/models/work_activity_notification_spec.rb
@@ -8,18 +8,144 @@ describe WorkActivityNotification, type: :model do
   let(:work_activity) { FactoryBot.create(:work_activity, work:) }
   let(:notification_mailer) { instance_double(NotificationMailer) }
   let(:message_delivery) { instance_double(ActionMailer::Parameterized::MessageDelivery) }
+  let(:reject_delivery) { instance_double(ActionMailer::Parameterized::MessageDelivery) }
 
   describe ".new" do
     before do
       allow(message_delivery).to receive(:deliver_later)
       allow(notification_mailer).to receive(:build_message).and_return(message_delivery)
-      allow(notification_mailer).to receive(:reject_message).and_return(message_delivery)
+      allow(notification_mailer).to receive(:reject_message).and_return(reject_delivery)
       allow(NotificationMailer).to receive(:with).and_return(notification_mailer)
     end
 
     it "enqueues an e-mail message to be delivered for the notification" do
       described_class.create(user:, work_activity:)
       expect(message_delivery).to have_received(:deliver_later)
+    end
+
+    context "when the notification is for a Draft submission" do
+      let(:work) { FactoryBot.create(:draft_work, group:) }
+      let(:new_delivery) { instance_double(ActionMailer::Parameterized::MessageDelivery) }
+
+      before do
+        allow(notification_mailer).to receive(:new_submission_message).and_return(new_delivery)
+        allow(new_delivery).to receive(:deliver_later)
+      end
+
+      it "enqueues an e-mail the news submission message to be delivered for the notification" do
+        described_class.create(user:, work_activity:)
+        expect(message_delivery).not_to have_received(:deliver_later)
+        expect(new_delivery).to have_received(:deliver_later)
+      end
+
+      context "when the notification is a user message" do
+        let(:work_activity) { FactoryBot.create(:work_activity_message, work:, message: "new submission created") }
+
+        it "enqueues an e-mail message to be delivered for the notification" do
+          described_class.create(user:, work_activity:)
+          expect(message_delivery).to have_received(:deliver_later)
+          expect(new_delivery).not_to have_received(:deliver_later)
+        end
+      end
+    end
+
+    context "when the notification is for a awaiting approval submission" do
+      let(:work) do
+        work = FactoryBot.create(:awaiting_approval_work, group:)
+        UserWork.create(user_id: user.id, work_id: work.id, state: "draft")
+        UserWork.create(user_id: user.id, work_id: work.id, state: "awaiting_approval")
+        work
+      end
+      let(:await_delivery) { instance_double(ActionMailer::Parameterized::MessageDelivery) }
+
+      before do
+        allow(notification_mailer).to receive(:review_message).and_return(await_delivery)
+        allow(await_delivery).to receive(:deliver_later)
+      end
+
+      it "enqueues an e-mail the news submission message to be delivered for the notification" do
+        described_class.create(user:, work_activity:)
+        expect(message_delivery).not_to have_received(:deliver_later)
+        expect(await_delivery).to have_received(:deliver_later)
+      end
+
+      context "when the notification is a user message" do
+        let(:work_activity) { FactoryBot.create(:work_activity_message, work:, message: "new submission created") }
+
+        it "enqueues an e-mail message to be delivered for the notification" do
+          described_class.create(user:, work_activity:)
+          expect(message_delivery).to have_received(:deliver_later)
+          expect(await_delivery).not_to have_received(:deliver_later)
+        end
+      end
+    end
+
+    context "when the notification is for a rejected submission" do
+      let(:work) do
+        work = FactoryBot.create(:draft_work, group:)
+        UserWork.create(user_id: user.id, work_id: work.id, state: "draft")
+        UserWork.create(user_id: user.id, work_id: work.id, state: "awaiting_approval")
+        UserWork.create(user_id: user.id, work_id: work.id, state: "draft")
+        work
+      end
+      let(:reject_delivery) { instance_double(ActionMailer::Parameterized::MessageDelivery) }
+
+      before do
+        allow(notification_mailer).to receive(:reject_message).and_return(reject_delivery)
+        allow(reject_delivery).to receive(:deliver_later)
+      end
+
+      it "enqueues an e-mail the news submission message to be delivered for the notification" do
+        described_class.create(user:, work_activity:)
+        expect(message_delivery).not_to have_received(:deliver_later)
+        expect(reject_delivery).to have_received(:deliver_later)
+      end
+
+      context "when the notification is a user message" do
+        let(:work_activity) { FactoryBot.create(:work_activity_message, work:, message: "new submission created") }
+
+        it "enqueues an e-mail message to be delivered for the notification" do
+          described_class.create(user:, work_activity:)
+          expect(message_delivery).to have_received(:deliver_later)
+          expect(reject_delivery).not_to have_received(:deliver_later)
+        end
+      end
+    end
+
+    context "when the notification is for an approved submission" do
+      let(:work) do
+        work = FactoryBot.create(:draft_work, group:)
+        work.state = "awaiting_approval"
+        work.save!
+        WorkActivity.add_work_activity(work.id, "marked as #{work.state.to_s.titleize}", user.id, activity_type: WorkActivity::SYSTEM)
+        work.state = "approved"
+        work.save!
+        WorkActivity.add_work_activity(work.id, "marked as #{work.state.to_s.titleize}", user.id, activity_type: WorkActivity::SYSTEM)
+        work
+      end
+      let(:approve_delivery) { instance_double(ActionMailer::Parameterized::MessageDelivery) }
+
+      before do
+        pending "there is no separate approval message yet"
+        allow(notification_mailer).to receive(:approve_message).and_return(approve_delivery)
+        allow(approve_delivery).to receive(:deliver_later)
+      end
+
+      it "enqueues an e-mail the news submission message to be delivered for the notification" do
+        described_class.create(user:, work_activity:)
+        expect(message_delivery).not_to have_received(:deliver_later)
+        expect(approve_delivery).to have_received(:deliver_later)
+      end
+
+      context "when the notification is a user message" do
+        let(:work_activity) { FactoryBot.create(:work_activity_message, work:, message: "new submission created") }
+
+        it "enqueues an e-mail message to be delivered for the notification" do
+          described_class.create(user:, work_activity:)
+          expect(message_delivery).to have_received(:deliver_later)
+          expect(approve_delivery).not_to have_received(:deliver_later)
+        end
+      end
     end
 
     context "when e-mail notifications are disabled for the Group" do
@@ -32,7 +158,7 @@ describe WorkActivityNotification, type: :model do
         expect(message_delivery).not_to have_received(:deliver_later)
       end
 
-      context "a messge notification" do
+      context "a message notification" do
         let(:work_activity) { FactoryBot.create(:work_activity_message, work:, message: "direct message to @#{user.uid}") }
 
         it "does enqueue an e-mail message to be delivered for the notification" do
@@ -41,7 +167,7 @@ describe WorkActivityNotification, type: :model do
         end
       end
 
-      context "a messge notification without an @" do
+      context "a message notification without an @" do
         let(:work_activity) { FactoryBot.create(:work_activity_message, work:) }
 
         it "does not enqueue an e-mail message to be delivered for the notification" do
@@ -59,7 +185,7 @@ describe WorkActivityNotification, type: :model do
         expect(message_delivery).not_to have_received(:deliver_later)
       end
 
-      context "a messge notification" do
+      context "a message notification" do
         let(:work_activity) { FactoryBot.create(:work_activity_message, work:, message: "direct message to @#{user.uid}") }
 
         it "does enqueue an e-mail message to be delivered for the notification" do
@@ -68,7 +194,7 @@ describe WorkActivityNotification, type: :model do
         end
       end
 
-      context "a messge notification without an @" do
+      context "a message notification without an @" do
         let(:work_activity) { FactoryBot.create(:work_activity_message, work:) }
 
         it "does not enqueue an e-mail message to be delivered for the notification" do
@@ -86,7 +212,7 @@ describe WorkActivityNotification, type: :model do
         group.add_submitter(super_admin, user)
       end
 
-      context "a messge notification" do
+      context "a message notification" do
         let(:work_activity) { FactoryBot.create(:work_activity_message, work:, message: "direct message to @#{user.uid}") }
 
         it "does enqueue an e-mail message to be delivered for the notification" do
@@ -95,7 +221,7 @@ describe WorkActivityNotification, type: :model do
         end
       end
 
-      context "a messge notification without an @" do
+      context "a message notification without an @" do
         let(:work_activity) { FactoryBot.create(:work_activity_message, work:) }
 
         it "does enqueue an e-mail message to be delivered for the notification" do
