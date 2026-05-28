eced5b16ad769c879338f6f14aefbc706bdcf215
Carolyn Cole <cac9@princeton.edu>
2026-05-28 11:15:40 -0400

    Allows search by first name in messaging @ Adds controller test to add some more detailed tests fixes #2233

--- app/controllers/user_lookup_controller.rb
+++ app/controllers/user_lookup_controller.rb
@@ -4,7 +4,8 @@ class UserLookupController < ApplicationController
     term = params[:term]
     users = User.where("uid ILIKE ?", "#{term}%")
                 .or(User.where("family_name ILIKE ?", "#{term}%"))
-                .order(:uid).limit(20)
+                .or(User.where("given_name ILIKE ?", "#{term}%"))
+                .order(:full_name).limit(20)
     user_data = users.map { |user| { uid: user.uid, name: user.full_name_safe } }
     render json: user_data.to_json
   end
