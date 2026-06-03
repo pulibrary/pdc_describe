db8696d6ac5811669449a5e568cd3031771ebcf6
Carolyn Cole <cac9@princeton.edu>
2026-06-03 16:23:59 -0400

    Updating deletion marker resubmit to be the correct class name

--- app/models/work_state_transition/deletion_marker_notification.rb
+++ app/models/work_state_transition/deletion_marker_notification.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 module WorkStateTransition
-  class WithdrawnNotification < BaseNotification
+  class DeletionMarkerNotification < BaseNotification
   end
 end
--- app/models/work_state_transition/resubmission_notification.rb
+++ app/models/work_state_transition/resubmission_notification.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 module WorkStateTransition
-  class WithdrawnNotification < BaseNotification
+  class ResubmissionNotification < BaseNotification
   end
 end
