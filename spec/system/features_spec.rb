e713bf3d3988c7efe3fb413e394845d15380c156
Precilla Prempeh <pprempeh@princeton.edu>
2026-06-26 14:59:20 -0400

    Create test for feature flipper Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>

---
+++ spec/system/features_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "features", type: :system, js: true do
+  it "flip flop doesn't show for un logged in user" do
+    visit "/features"
+    expect(page).to have_content("Log in")
+  end
+
+  it "flip flop doesn't show for a regular user" do
+    user = FactoryBot.create(:user)
+    sign_in user
+    visit "/features"
+    expect(page).not_to have_content("Create a dataset")
+  end
+
+
+end
